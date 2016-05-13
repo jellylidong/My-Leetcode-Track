799c9f478cac466850cad711ab2a4c0d977366fc
Dong <jellylidong@users.noreply.github.com>
2016-05-12 23:07:30 -0400

    [142. Linked List Cycle II][Accepted]committed by LeetCode Extension

--- 142-Linked-List-Cycle-II/solution.java
+++ 142-Linked-List-Cycle-II/solution.java
@@ -9,6 +9,24 @@
  *     }
  * }
  */
+ 
+ /*explain
+ assume there are l nodes before circle, and there are c nodes in the circle
+ slow goes one node each step, fast goes two node each step
+ after p steps they meet
+ then (p-l)%c == (2p-l)%c
+ because they are in the same position of the circle
+ (2p-l)%c = p%c + (p-l)%c == (p-l)%c ==> p%c == 0;
+ so (p-l)%c = (-l)%c, which means current position is l nodes from the beginning
+ node:don't worry about l  > c or l < c, actually, no matter what l is has nothing to do with the beginning
+ position of the circle, so we can simply assume the l < c.
+ 
+ so after slow and fast meet
+ set slow to head, let each pointer go on node each step
+ then the meet node is the starting node of the circle
+ i.e the two pointers both goes l nodes
+ */
+ 
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         ListNode slow = head;
