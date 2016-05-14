2a9add301a95ed66d5b37b77a73086b45086a064
Dong <jellylidong@users.noreply.github.com>
2016-05-14 16:50:40 -0400

    [242. Valid Anagram][Runtime Error]committed by LeetCode Extension

---
+++ 242-Valid-Anagram/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+            return false;
+        
+        int[] count = new int[27];
+        
+        for(int i = 0 ; i < s.length(); i++){
+            char cs = s.charAt(i);
+            char ts = t.charAt(i);
+            count[cs-'a']++;
+            count[ts-'a']--;
+        }
+        
+        for(int i = 0; i < s.length(); i++){
+            if(count[i] < 0)
+                return false;
+        }
+        
+        return true;
+    }
+}
