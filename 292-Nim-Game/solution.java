3320444075a3219f3e5edaacacd390d1a57ac55c
Dong <jellylidong@users.noreply.github.com>
2016-05-14 15:31:34 -0400

    [292. Nim Game][Accepted]committed by LeetCode Extension

--- 292-Nim-Game/solution.java
+++ 292-Nim-Game/solution.java
@@ -1,24 +1,6 @@
 public class Solution {
     public boolean canWinNim(int n) {
         
-        if(n <= 3)
-            return true;
-        if(n == 4)
-            return false;
-            
-        boolean[] dp = new boolean[4];
-        dp[0] = dp[1] = dp[2] = true;
-        dp[3] = false;
-        
-        for(int i = 4; i < n; i++){
-            boolean dpi = dp[0] || dp[1] || dp[2];
-            dp[0] = dp[1];
-            dp[1] = dp[2];
-            dp[2] = dp[3];
-            dp[3] = dpi;
-            
-        }
-        
-        return dp[3];
+        return n%4 != 0;
     }
 }
