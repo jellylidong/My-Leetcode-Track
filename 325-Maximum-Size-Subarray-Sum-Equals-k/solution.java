d75fe49403f5decaf0c2c5b9b96f93aa913c3d1c
Dong <jellylidong@users.noreply.github.com>
2016-05-14 19:11:03 -0400

    [325. Maximum Size Subarray Sum Equals k][Accepted]committed by LeetCode Extension

--- 325-Maximum-Size-Subarray-Sum-Equals-k/solution.java
+++ 325-Maximum-Size-Subarray-Sum-Equals-k/solution.java
@@ -1,6 +1,7 @@
 public class Solution {
     public int maxSubArrayLen(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
+        //key is sum of [0...i], value is current index i
         int sum = 0;
         int max = 0;
         
@@ -9,6 +10,8 @@ public class Solution {
             if(sum == k)
                 max = Math.max(max, i+1); // i+1 always > previous max
             else if(map.containsKey(sum-k))
+            //map.containsKey(sum-k) means there is a position that key is sum-k (assume j)
+            //then sum of [j+1...i] is k, so a new length can be i- map.get(sum-k)
                 max = Math.max(max, i - map.get(sum-k));
                 
             if(!map.containsKey(sum))
