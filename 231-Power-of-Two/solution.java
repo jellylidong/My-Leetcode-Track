c2fc81aba0d19d2ac2a60d020a6ae441337d23ff
Dong <jellylidong@users.noreply.github.com>
2016-05-14 21:18:06 -0400

    [231. Power of Two][Runtime Error]committed by LeetCode Extension

--- 231-Power-of-Two/solution.java
+++ 231-Power-of-Two/solution.java
@@ -1,8 +1,10 @@
 public class Solution {
     public boolean isPowerOfTwo(int n) {
-        while(n%2 == 0 && n != 0)
-            n /= 2;
+        // while(n%2 == 0 && n != 0)
+        //     n /= 2;
             
-        return n == 1;
+        // return n == 1;
+        
+        return 1073741824%n == 0;
     }
 }
