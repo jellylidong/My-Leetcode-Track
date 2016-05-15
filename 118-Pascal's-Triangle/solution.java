407cc47d76eebaf335192df389d9db32989b7107
Dong <jellylidong@users.noreply.github.com>
2016-05-15 16:01:49 -0400

    [118. Pascal's Triangle][Accepted]committed by LeetCode Extension

--- 118-Pascal's-Triangle/solution.java
+++ 118-Pascal's-Triangle/solution.java
@@ -4,10 +4,10 @@ public class Solution {
         if(n <= 0)
             return ans;
             
-        for(int i = 1; i <= n; i++){
+        for(int i = 0; i < n; i++){
             List<Integer> list = new ArrayList<>();
-            for(int j=0; j < i; j++){
-                if(j==0 || j == i-1)
+            for(int j=0; j <= i; j++){
+                if(j==0 || j == i)
                     list.add(1);
                 else{
                     list.add(ans.get(i-1).get(j-1) + ans.get(i-1).get(j));
