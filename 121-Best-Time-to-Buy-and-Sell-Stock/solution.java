a2fcfef1be6443e7f9297e58bdda10ac1beede71
Dong <jellylidong@users.noreply.github.com>
2016-05-14 23:16:15 -0400

    [121. Best Time to Buy and Sell Stock][Runtime Error]committed by LeetCode Extension

--- 121-Best-Time-to-Buy-and-Sell-Stock/solution.java
+++ 121-Best-Time-to-Buy-and-Sell-Stock/solution.java
@@ -1,25 +1,14 @@
 public class Solution {
     public int maxProfit(int[] p) {
-        int maxId = -1;
-        int minId = -1;
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
+        int min = p[0];
         int ans = 0;
         
         for(int i = 0; i < p.length; i++){
-            if(p[i] >= max){
-                max = p[i];
-                maxId = i;
-            }
-            if(p[i] <= min){
+            if(p[i] < min){
                 min = p[i];
-                minId = i;
             }
-            if(maxId != -1 && minId != -1 && maxId < minId)
-                maxId = minId;
-            
-            if(maxId != -1 && minId != -1 && maxId > minId){
-                ans = Math.max(ans, p[maxId]-p[minId]);
+            else{
+                ans = Math.max(ans, p[i]-min);
             }
         }
         
