419ce759057d46247ed8b81b4bf84dc75bb698d2
Dong <jellylidong@users.noreply.github.com>
2016-05-15 16:58:52 -0400

    [172. Factorial Trailing Zeroes][Accepted]committed by LeetCode Extension

--- 172-Factorial-Trailing-Zeroes/solution.java
+++ 172-Factorial-Trailing-Zeroes/solution.java
@@ -9,3 +9,11 @@ public class Solution {
         return ans;
     }
 }
+
+//http://www.geeksforgeeks.org/count-trailing-zeroes-factorial-number/
+//for 5! there is only one 5 => one 0
+//for 11!, there are 5, 10 => 2 5s => two 0s
+// ==> the tail 0s are the count of n/5 + n/25 + n/125...
+//so we can let i = 5 then count += n/i, i*=5
+//consider that i*=5 may exceed the max integer
+//so use n/=5 instead
