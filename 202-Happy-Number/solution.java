f111adaea0f47c57ffbcf2baff7b3b9a4f6b40ab
Dong <jellylidong@users.noreply.github.com>
2016-05-14 22:34:13 -0400

    [202. Happy Number][Accepted]committed by LeetCode Extension

--- 202-Happy-Number/solution.java
+++ 202-Happy-Number/solution.java
@@ -1,25 +1,24 @@
 public class Solution {
     public boolean isHappy(int n) {
+        if(n <= 1)
+            return true;
+            
         HashSet<Integer> set = new HashSet<>();
         set.add(n);
         while(true){
             int sum = 0;
             int d = 1;
-            while(n/d != 0){
-                sum += Math.pow(n%d, 2);
+            
+            while(n != 0){
+                sum += Math.pow(n%10, 2);
+                n = n/10;
             }
-            if(set.contains(sum)){
-                if(sum == 1)
-                    return true;
-                else
-                    return false;
-            }
-            else{
-                set.add(sum);
-                n = sum;
-            }
+            if(sum == 1)
+                return true;
+            if(set.contains(sum))
+                return false;
+            set.add(sum);
+            n = sum;
         }
-        
-        // return false;
     }
 }
