3787e325a14e9f654a0d0676ee3bfd5a65ee875c
Dong <jellylidong@users.noreply.github.com>
2016-05-14 22:41:20 -0400

    [202. Happy Number][Accepted]committed by LeetCode Extension

--- 202-Happy-Number/solution2.java
+++ 202-Happy-Number/solution2.java
@@ -1,4 +1,14 @@
 public class Solution {
+    /*explain
+    similar to find a circle in a linked list
+    n is a node
+    then digit2Sum(n) will be the next node
+    
+    if slow and fast meet, it means there is a circle
+    if slow is 1, => happy
+    otherwise not
+    */
+    
     public boolean isHappy(int n) {
         int slow = n;
         int fast = n;
