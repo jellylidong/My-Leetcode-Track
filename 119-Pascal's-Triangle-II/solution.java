34111e20a6c8eb3cc17b89aa5ad38d1cf31ad7e5
Dong <jellylidong@users.noreply.github.com>
2016-05-15 17:26:25 -0400

    [119. Pascal's Triangle II][Accepted]committed by LeetCode Extension

--- 119-Pascal's-Triangle-II/solution.java
+++ 119-Pascal's-Triangle-II/solution.java
@@ -1,15 +1,20 @@
 public class Solution {
     public List<Integer> getRow(int n) {
         List<Integer> list = new ArrayList<>();
-        if(n == 0)
+        if(n < 0)
             return list;
         list.add(1); 
-        for(int i = 2; i <= n; i++){
-            for(int j = i; j > 0; j--){
-                if(j > list.size())
+        //start from 0, i means ith level
+        //start from 0, j means jth element
+        for(int i = 1; i <= n; i++){
+            for(int j=i; j > 0; j--){
+                if(j == list.size()){
+                    //
                     list.add(1);
-                else
-                    list.set(j-1, list.get(j-1) + list.get(j-2));
+                }
+                else{
+                    list.set(j, list.get(j)+list.get(j-1));
+                }
             }
         }
         
