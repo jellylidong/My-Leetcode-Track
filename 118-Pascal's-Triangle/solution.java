aa7eaeb401c1939b244adeb078f19483c52f95d3
Dong <jellylidong@users.noreply.github.com>
2016-05-15 15:59:36 -0400

    [118. Pascal's Triangle][Runtime Error]committed by LeetCode Extension

---
+++ 118-Pascal's-Triangle/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(n <= 0)
+            return ans;
+            
+        for(int i = 1; i <= n; i++){
+            List<Integer> list = new ArrayList<>();
+            for(int j=0; j < i; j++){
+                if(j==0 || j == i-1)
+                    list.add(1);
+                else{
+                    list.add(ans.get(i-1).get(j-1) + ans.get(i-1).get(j));
+                }
+            }
+            
+            ans.add(list);
+        }
+        return ans;
+    }
+}
