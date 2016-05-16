0963fcfa2007929fe96ea37e1ae791365d22b9b2
Dong <jellylidong@users.noreply.github.com>
2016-05-16 12:00:44 -0400

    [9. Palindrome Number][Accepted]committed by LeetCode Extension

--- 9-Palindrome-Number/solution.java
+++ 9-Palindrome-Number/solution.java
@@ -1,34 +1,41 @@
 public class Solution {
-    public boolean isPalindrome(int x) {
-        if(x < 0)
-            return false;
-            
-        if(x == 0)
-            return true;
-        int len = 0;
-        int tmp = x;
-        while(tmp != 0){
-            len++;
-            tmp /= 10;;
-        }
-        while(len > 0 && x > 0){
-            int lo = x%10;
-            int hi = x/((int)Math.pow(10, len-1));
-            if(hi != lo){
-                System.out.print(x);
-                return false;
-            }
-            x -= lo;
-            x -= hi*(int)Math.pow(10, len-1);
-            x /= 10;
-            len -= 2;
-        }
-        
-        return true;
-    }
-    
     // public boolean isPalindrome(int x) {
-    
+    //     if(x < 0)
+    //         return false;
+            
+    //     if(x == 0)
+    //         return true;
+    //     int len = 0;
+    //     int tmp = x;
+    //     while(tmp != 0){
+    //         len++;
+    //         tmp /= 10;;//use / instead of * to avoid overflow
+    //     }
+    //     while(len > 0 && x > 0){
+    //         int lo = x%10;
+    //         int hi = x/((int)Math.pow(10, len-1));
+    //         if(hi != lo){
+    //             // System.out.print(x);
+    //             return false;
+    //         }
+    //         x -= lo;
+    //         x -= hi*(int)Math.pow(10, len-1);
+    //         x /= 10;
+    //         len -= 2;
+    //     }
         
+    //     return true;
     // }
+    
+    public boolean isPalindrome(int x) {
+    
+        int p = x;
+        int q = 0;
+        while(p > 0){
+            q = q*10;
+            q += p%10;
+            p /= 10;
+        }
+        return q == x;
+    }
 }
