7107d94acac97bac96c2e7fb046044c26ce972ac
Dong <jellylidong@users.noreply.github.com>
2016-05-18 01:03:49 -0400

    [219. Contains Duplicate II][Runtime Error]committed by LeetCode Extension

--- 219-Contains-Duplicate-II/solution.java
+++ 219-Contains-Duplicate-II/solution.java
@@ -1,5 +1,7 @@
 public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if(k <= 0 || k >= nums.length)
+            return false;
         HashSet<Integer> set = new HashSet<>();
         for(int i = 0; i < nums.length; i++){
             if(i >= k){
