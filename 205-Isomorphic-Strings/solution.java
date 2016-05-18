b13515edb7ae0e24616d0280e9c82e5ca4e5bc03
Dong <jellylidong@users.noreply.github.com>
2016-05-18 12:58:12 -0400

    [205. Isomorphic Strings][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 205-Isomorphic-Strings/solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int sl = s.length();
+        int tl = t.length();
+        if(sl != tl)
+            return false;
+        
+        return encoding(s).equals(encoding(t));
+        
+        
+        
+    }
+    
+    public String encoding(String s){
+        HashMap<Character, Integer> map = new HashMap<>();
+        String res = "";
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(map.containsKey(c)){
+            }
+            else{
+                map.put(c, i);
+            }
+            res += map.get(c);
+        }
+        
+        return res;
+    }
+}
