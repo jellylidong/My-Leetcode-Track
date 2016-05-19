97bb1ff96b904897bc4678a7db9c55a1d6deb56c
Dong <jellylidong@users.noreply.github.com>
2016-05-19 15:17:43 -0400

    [170. Two Sum III - Data structure design][Accepted]committed by LeetCode Extension

--- 170-Two-Sum-III---Data-structure-design/solution.java
+++ 170-Two-Sum-III---Data-structure-design/solution.java
@@ -1,16 +1,26 @@
 public class TwoSum {
+    //!!!!!
+    //note: when using hashmap.keySet()
+    //it first need to trasfer all KV pairs to a set
+    //this is very time consuming
+    //to save time, we can use a list to store all elements
+    //and iterate all elements vai list
+    
     HashMap<Integer, Integer> map = new HashMap<>();
+    List<Integer> list = new ArrayList<>();
     // Add the number to an internal data structure.
 	public void add(int number) {
 	    if(map.containsKey(number))
 	        map.put(number, map.get(number) + 1);
-	    else
+	    else{
 	        map.put(number, 1);
+	        list.add(number);
+	    }
 	}
 
     // Find if there exists any pair of numbers which sum is equal to the value.
 	public boolean find(int value) {
-	    for(Integer n: map.keySet()){
+	    for(Integer n: list){
 	        if(map.containsKey(value-n)){
 	            if(value-n == n && map.get(n) > 1)
 	                return true;
