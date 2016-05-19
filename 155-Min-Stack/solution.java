902e31d2b24a5cd815e292d8cebd8d1d9c391cd4
Dong <jellylidong@users.noreply.github.com>
2016-05-19 17:05:54 -0400

    [155. Min Stack][Runtime Error]committed by LeetCode Extension

---
+++ 155-Min-Stack/solution.java
@@ -0,0 +1,42 @@
+public class MinStack {
+
+    /** initialize your data structure here. */
+    Stack<Integer> stack;
+    int min;
+    public MinStack() {
+        stack = new Stack<>();
+        min = Integer.MAX_VALUE;
+    }
+    
+    public void push(int x) {
+        min = Math.min(min, x);
+        stack.push(x);
+        stack.push(min);
+    }
+    
+    public void pop() {
+        stack.pop();
+        stack.pop();
+        min = stack.peek();
+    }
+    
+    public int top() {
+        int tmp = stack.pop();
+        int top = stack.peek();
+        stack.push(tmp);
+        return top;
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
