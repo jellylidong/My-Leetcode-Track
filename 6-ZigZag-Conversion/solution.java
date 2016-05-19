283c3b4d4ef163ead370ce8bda966817784b57cf
Dong <jellylidong@users.noreply.github.com>
2016-05-19 14:58:07 -0400

    [6. ZigZag Conversion][Accepted]committed by LeetCode Extension

--- 6-ZigZag-Conversion/solution.java
+++ 6-ZigZag-Conversion/solution.java
@@ -5,15 +5,11 @@ public class Solution {
         String[] ss = new String[n];
         for(int i = 0; i < n; i++)
             ss[i] = "";
-        int id = 0;
+        
         for(int i = 0; i < s.length(); i++){
-            int di = n+n-2;
-            if(di == 0)
-                id = i;
-            else
-                id = i%di;
+            int id = i%(n+n-2);
             if(id >= n){
-                id = n - id;
+                id = n+n-2-id;
             }
             ss[id] += s.charAt(i);
         }
