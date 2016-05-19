2ffceea2b7846de9c2fd9496af6e48b2902dab28
Dong <jellylidong@users.noreply.github.com>
2016-05-19 16:56:11 -0400

    [349. Intersection of Two Arrays][Accepted]committed by LeetCode Extension

---
+++ 349-Intersection-of-Two-Arrays/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums1.length; i++)
+            set.add(nums1[i]);
+        HashSet<Integer> ans = new HashSet<>();
+        for(int i = 0;  i < nums2.length; i++){
+            if(set.contains(nums2[i]))
+                ans.add(nums2[i]);
+        }
+        
+        int size = ans.size();
+        int [] arr = new int[size];
+        for(int n: ans){
+            arr[size-1] = n;
+            size--;
+        }
+        
+        return arr;
+    }
+}
