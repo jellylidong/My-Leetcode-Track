bfd10a8dd8e4f3ab75d8020ca6ae01efedfbbd9e
Dong <jellylidong@users.noreply.github.com>
2016-05-19 23:16:41 -0400

    [189. Rotate Array][Runtime Error]committed by LeetCode Extension

--- 189-Rotate-Array/solution.java
+++ 189-Rotate-Array/solution.java
@@ -1,18 +1,17 @@
 public class Solution {
+    ///!!!!!!!!!!!!!!!!!!!!
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        //swap first k with last k numbers
-        for(int i = 0; i < k; i++){
-            int tmp = nums[i];
-            nums[i] = nums[n-1-i];
-            nums[n-1-i] = tmp;
-        }
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
         
-        //reverse last n-k numbers
-        for(int i = k; k < k + (n-k)/2; k++){
+    }
+    
+    public void reverse(int[] nums, int lo, int hi){
+        for(int i = lo; i <= lo + (hi-lo)/2; i++){
             int tmp = nums[i];
-            nums[i] = nums[n-1-i];
-            nums[n-1-i] = tmp;
+            nums[i] = nums[hi-(i-lo)];
+            nums[hi-(i-lo)] = tmp;
         }
     }
 }
@@ -21,7 +20,7 @@ public class Solution {
 
 //m2 store the last k numbers, move k position for one time and use the stored array to modify the first k position
 
-//m3 swap first k numbers and last k numbers, ie swap(1, k-1), swap(2, k-2) ...., 
+//?????? m3 swap first k numbers and last k numbers, ie swap(1, k-1), swap(2, k-2) ...., 
 //after swap done, the first k numbers are in correct position, but the the rest n-k numbers are reversed, we need to reverse it
 
 //m4 revers first n-k numbers, last k numbers, then all numbers
