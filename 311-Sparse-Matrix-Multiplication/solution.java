397dd777d70366d90098c8d681c27cc235e569aa
Dong <jellylidong@users.noreply.github.com>
2016-05-23 14:14:32 -0400

    [311. Sparse Matrix Multiplication][Accepted]committed by LeetCode Extension

--- 311-Sparse-Matrix-Multiplication/solution.java
+++ 311-Sparse-Matrix-Multiplication/solution.java
@@ -1,7 +1,13 @@
 public class Solution {
+    boolean[] zA;
+    boolean[] zB;
     public int[][] multiply(int[][] A, int[][] B) {
         int m = A.length;
         int l = B[0].length;
+        
+        zA = new boolean[m];
+        zB = new boolean[l];
+        
         int[][] ans = new int[m][l];
         
         for(int i = 0; i < m; i++){
@@ -14,10 +20,25 @@ public class Solution {
     }
     
     public int mult(int[][] A, int ra, int[][] B, int lb){
+        if(zA[ra] || zB[lb])
+            return 0;
+    
+        int countA = 0;
+        int countB = 0;
+        
         int ans = 0;
         for(int i = 0; i < A[ra].length; i++){
             ans += A[ra][i] * B[i][lb];
+            if(A[ra][i] == 0)
+                countA++;
+            if(B[i][lb] == 0)
+                countB++;
         }
+        
+        if(countA == A[ra].length)
+            zA[ra] = true;
+        if(countB == B.length)
+            zB[lb] = true;
         
         return ans;
     }
