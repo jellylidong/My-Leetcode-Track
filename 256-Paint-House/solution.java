84e5e73e494c945c83feadf2db04f1ca389ad160
Dong <jellylidong@users.noreply.github.com>
2016-05-23 17:52:28 -0400

    [256. Paint House][Runtime Error]committed by LeetCode Extension

---
+++ 256-Paint-House/solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int minCost(int[][] costs) {
+        int[][] ans = new int[costs.length][3];
+        ans[0][0] = costs[0][0];
+        ans[0][1] = costs[0][1];
+        ans[0][2] = costs[0][2];
+        
+        for(int i = 1; i < costs.length; i++){
+            for(int j = 0; j < 3; j++){
+                int min = Integer.MAX_VALUE;
+                for(int k = 0; k < 3; j++){
+                    if(k != j)
+                        min = Math.min(costs[i-1][k], min);
+                }
+                ans[i][j] = min + ans[i-1][j];
+            }
+        }
+        
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < 3; i++)
+            min = Math.min(min, ans[costs.length-1][i]);
+            
+        return min;
+    }
+}
