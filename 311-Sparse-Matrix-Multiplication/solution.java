ebd03cbe47c97626a21938a3a5fa76a0994c6be3
Dong <jellylidong@users.noreply.github.com>
2016-05-23 14:30:03 -0400

    [311. Sparse Matrix Multiplication][Accepted]committed by LeetCode Extension

--- 311-Sparse-Matrix-Multiplication/solution.java
+++ 311-Sparse-Matrix-Multiplication/solution.java
@@ -1,44 +1,65 @@
 public class Solution {
-    boolean[] zA;
-    boolean[] zB;
+    
+    // //method1
+    // boolean[] zA;
+    // boolean[] zB;
+    // public int[][] multiply(int[][] A, int[][] B) {
+    //     int m = A.length;
+    //     int l = B[0].length;
+        
+    //     zA = new boolean[m];
+    //     zB = new boolean[l];
+        
+    //     int[][] ans = new int[m][l];
+        
+    //     for(int i = 0; i < m; i++){
+    //         for(int j = 0; j < l; j++){
+    //             ans[i][j] = mult(A, i, B, j);
+    //         }
+    //     }
+        
+    //     return ans;
+    // }
+    
+    // public int mult(int[][] A, int ra, int[][] B, int lb){
+    //     if(zA[ra] || zB[lb]) //to save time, if the whole row of A is 0 or the whole column is 0, skip it
+    //         return 0;
+    
+    //     int countA = 0;
+    //     int countB = 0;
+        
+    //     int ans = 0;
+    //     for(int i = 0; i < A[ra].length; i++){
+    //         ans += A[ra][i] * B[i][lb];
+    //         if(A[ra][i] == 0)
+    //             countA++;
+    //         if(B[i][lb] == 0)
+    //             countB++;
+    //     }
+        
+    //     if(countA == A[ra].length)
+    //         zA[ra] = true;
+    //     if(countB == B.length)
+    //         zB[lb] = true;
+        
+    //     return ans;
+    // }
+    
+    //method 2
     public int[][] multiply(int[][] A, int[][] B) {
         int m = A.length;
         int l = B[0].length;
-        
-        zA = new boolean[m];
-        zB = new boolean[l];
-        
         int[][] ans = new int[m][l];
         
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < l; j++){
-                ans[i][j] = mult(A, i, B, j);
+        for(int i = 0; i < A.length; i++){
+            for(int j = 0; j < A[i].length; j++){
+                if(A[i][j] != 0){
+                    for(int k = 0; k < B[0].length; k++){
+                        ans[i][k] += A[i][j] * B[j][k];
+                    }
+                }
             }
         }
-        
-        return ans;
-    }
-    
-    public int mult(int[][] A, int ra, int[][] B, int lb){
-        if(zA[ra] || zB[lb]) //to save time, if the whole row of A is 0 or the whole column is 0, skip it
-            return 0;
-    
-        int countA = 0;
-        int countB = 0;
-        
-        int ans = 0;
-        for(int i = 0; i < A[ra].length; i++){
-            ans += A[ra][i] * B[i][lb];
-            if(A[ra][i] == 0)
-                countA++;
-            if(B[i][lb] == 0)
-                countB++;
-        }
-        
-        if(countA == A[ra].length)
-            zA[ra] = true;
-        if(countB == B.length)
-            zB[lb] = true;
         
         return ans;
     }
