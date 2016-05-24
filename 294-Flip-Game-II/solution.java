9fe91056adc54651a361d0aa301717cde4e81efc
Dong <jellylidong@users.noreply.github.com>
2016-05-24 03:10:04 -0400

    [294. Flip Game II][Accepted]committed by LeetCode Extension

--- 294-Flip-Game-II/solution.java
+++ 294-Flip-Game-II/solution.java
@@ -8,7 +8,7 @@ public class Solution {
             if(s.charAt(i) == '+' && s.charAt(i+1) == '+'){
                 // boolean left = canWin(s.substring(0, i));
                 // boolean right = canWin(s.substring(i+2));
-                res = res || !canWin(s.substring(0,i) + "--" + s.substring(i+1));
+                res = res || !canWin(s.substring(0,i) + "--" + s.substring(i+2));
             }
             if(res)
                 break;
