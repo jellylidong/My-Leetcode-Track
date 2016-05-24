b9269350f2940048b2d02ca29048ba4e290de2f0
Dong <jellylidong@users.noreply.github.com>
2016-05-24 12:54:21 -0400

    [318. Maximum Product of Word Lengths][Accepted]committed by LeetCode Extension

--- 318-Maximum-Product-of-Word-Lengths/solution.java
+++ 318-Maximum-Product-of-Word-Lengths/solution.java
@@ -1,29 +1,30 @@
+/*instead of using hashset to record the chars that a string has
+here use bit manipulation
+ie for words[i], for a char in it, mask[i] |= 1 << (words[i].charAt(j)-'a')
+then we can judge if two strings share chars by mask[i] & mask[j] == 0
+*/
+
 public class Solution {
     public int maxProduct(String[] words) {
-        int ans = 0;
-        for(int i = 0; i < words.length; i++){
-            HashSet<Character> set = new HashSet<>();
-            for(int k = 0; k < words[i].length(); k++){
-                set.add(words[i].charAt(k));
+        int len = words.length;
+        int[] mask = new int[len];
+        
+        for(int i = 0; i < len; i++){
+            for(char c: words[i].toCharArray()){
+                mask[i] |= 1 << (c-'a');
             }
-            for(int j = i+1; j < words.length; j++){
-                if(!hasSharedChar(set, words[j])){
+        }
+        
+        int ans = 0;
+        for(int i = 0; i < len; i++){
+            for(int j = i+1; j < len; j++){
+                if((mask[i] & mask[j]) == 0) //bit calculation has lower priority than logic calculation, bracket it
                     ans = Math.max(ans, words[i].length() * words[j].length());
-                }
             }
         }
         
         return ans;
     }
     
-    public boolean hasSharedChar(HashSet<Character> set, String s2){
-        
-        for(int i = 0; i < s2.length(); i++){
-            if(set.contains(s2.charAt(i)))
-                return true;
-        }
-        
-        return false;
-            
-    }
+    
 }
