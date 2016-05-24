e44acc3f1ce3bc6b4683ec4d19ee15a0d5bccefb
Dong <jellylidong@users.noreply.github.com>
2016-05-24 12:36:21 -0400

    [318. Maximum Product of Word Lengths][Time Limit Exceeded]committed by LeetCode Extension

--- 318-Maximum-Product-of-Word-Lengths/solution.java
+++ 318-Maximum-Product-of-Word-Lengths/solution.java
@@ -2,8 +2,12 @@ public class Solution {
     public int maxProduct(String[] words) {
         int ans = 0;
         for(int i = 0; i < words.length; i++){
+            HashSet<Character> set = new HashSet<>();
+            for(int k = 0; k < words[i].length(); k++){
+                set.add(words[i].charAt(k));
+            }
             for(int j = i+1; j < words.length; j++){
-                if(!hasSharedChar(words[i], words[j])){
+                if(!hasSharedChar(set, words[j])){
                     ans = Math.max(ans, words[i].length() * words[j].length());
                 }
             }
@@ -12,11 +16,8 @@ public class Solution {
         return ans;
     }
     
-    public boolean hasSharedChar(String s1, String s2){
-        HashSet<Character> set = new HashSet<>();
-        for(int i = 0; i < s1.length(); i++){
-            set.add(s1.charAt(i));
-        }
+    public boolean hasSharedChar(HashSet<Character> set, String s2){
+        
         for(int i = 0; i < s2.length(); i++){
             if(set.contains(s2.charAt(i)))
                 return true;
