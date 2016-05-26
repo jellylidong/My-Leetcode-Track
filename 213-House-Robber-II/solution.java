d6a9e7ed97dfd033b55b3a9dca909df5b3657f8e
Dong <jellylidong@users.noreply.github.com>
2016-05-25 21:24:51 -0400

    [213. House Robber II][Accepted]committed by LeetCode Extension

--- 213-House-Robber-II/solution.java
+++ 213-House-Robber-II/solution.java
@@ -1,21 +1,23 @@
 public class Solution {
     public int rob(int[] nums) {
-        int len = nums.length-1;
-        if(len < 0)
+        int len = nums.length;
+        if(len == 0)
             return 0;
-        int r1 = helper(nums, 0, len-1);
-        int r2 = helper(nums, 1, len);
+        if(len == 1)
+            return nums[0];
+            
+        int r1 = helper(nums, 0, len-2);
+        int r2 = helper(nums, 1, len-1);
         
         return Math.max(r1, r2);
     }
     
     public int helper(int[] nums, int lo, int hi){
-        if(lo < hi)
-            return 0;
+        
         int in = nums[lo];
         int ex = 0;
         
-        for(int i = lo; i <= hi; i++){
+        for(int i = lo+1; i <= hi; i++){
             int tmp = in;
             in = Math.max(ex+nums[i], in);
             ex = Math.max(ex, tmp);
