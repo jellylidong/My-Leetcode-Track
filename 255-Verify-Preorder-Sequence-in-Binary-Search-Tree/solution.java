6d640705e3c9a95d30cec0bc5f7a60ebcedb3e37
Dong <jellylidong@users.noreply.github.com>
2016-05-27 14:30:30 -0400

    [255. Verify Preorder Sequence in Binary Search Tree][Accepted]committed by LeetCode Extension

--- 255-Verify-Preorder-Sequence-in-Binary-Search-Tree/solution.java
+++ 255-Verify-Preorder-Sequence-in-Binary-Search-Tree/solution.java
@@ -1,15 +1,34 @@
 public class Solution {
+    // public boolean verifyPreorder(int[] pre) {
+    //     int parent = Integer.MIN_VALUE;
+    //     int parentid = -1;
+    //     for(int i = 0; i < pre.length; i++){
+    //         if(pre[i] < parent)
+    //             return false;
+    //         while(parentid >= 0 && pre[i] > pre[parentid]){
+    //             // parentid--;
+    //             parent = pre[parentid];
+    //             parentid--;
+    //         }
+    //         parentid++;
+    //         pre[parentid] = pre[i];
+            
+    //     }
+        
+    //     return true;
+    // }
+    
     public boolean verifyPreorder(int[] pre) {
         int parent = Integer.MIN_VALUE;
-        int parentid = -1;
-        for(int i = 0; i < pre.length; i++){
-            if(pre[i] < parent)
+        Stack<Integer> stk = new Stack<>();
+        
+        for(int n: pre){
+            if(n < parent)
                 return false;
-            while(parentid >= 0 && pre[i] > pre[parentid]){
-                // parentid--;
-                parent = pre[parentid--];
-            }
-            pre[++parentid] = pre[i];
+            
+            while(!stk.isEmpty() && n > stk.peek())
+                parent = stk.pop();
+            stk.push(n);
         }
         
         return true;
