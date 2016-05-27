3ed8b85d5af2d80aef70c4f82396434465c62426
Dong <jellylidong@users.noreply.github.com>
2016-05-27 14:52:41 -0400

    [22. Generate Parentheses][Accepted]committed by LeetCode Extension

--- 22-Generate-Parentheses/solution.java
+++ 22-Generate-Parentheses/solution.java
@@ -6,6 +6,7 @@ public class Solution {
         return ans;
     }
     
+    //left and right are the number of left and right bracket
     public void generateParenthesis(int left, int right, String cur){
         if(left == 0 && right == 0){
             ans.add(cur);
@@ -15,9 +16,12 @@ public class Solution {
             generateParenthesis(left-1, right, "(");
         else{
             //if we want to put a "(" next, 
-            //we must guarantee that the left backet number >= 1 and right bracket number 
+            //we must guarantee that the left backet number >= 1 and right bracket number >= left-1
             if(left-1 >= 0 && right >= left-1)
                 generateParenthesis(left-1, right, cur+"(");
+                
+            //if we want to put a ")" next,
+            //we must guarantee that right bracket number >= 1 and right-1 >= left
             if(right-1 >= 0 && right-1 >= left)
                 generateParenthesis(left, right-1, cur+")");
         }
