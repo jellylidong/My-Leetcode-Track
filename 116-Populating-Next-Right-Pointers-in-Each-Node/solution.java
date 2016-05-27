1e738b2b3f9bd29eeea688d6b2d1bc82b0b90831
Dong <jellylidong@users.noreply.github.com>
2016-05-27 19:11:15 -0400

    [116. Populating Next Right Pointers in Each Node][Runtime Error]committed by LeetCode Extension

---
+++ 116-Populating-Next-Right-Pointers-in-Each-Node/solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        Queue<TreeLinkNode> q = new LinkedList<>();
+        q.offer(root);
+        
+        while(!q.isEmpty()){
+            TreeLinkNode pre = null;
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeLinkNode cur = q.poll();
+                if(cur.right != null)
+                    q.offer(cur.right);
+                if(cur.left != null)
+                    q.offer(cur.left);
+                cur.next = pre;
+                pre = cur;
+            }
+        }
+    }
+}
