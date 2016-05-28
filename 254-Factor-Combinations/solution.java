846283ccf85407ba7d360ed38b4c5fb201cce7b7
Dong <jellylidong@users.noreply.github.com>
2016-05-28 18:20:09 -0400

    [254. Factor Combinations][Accepted]committed by LeetCode Extension

---
+++ 254-Factor-Combinations/solution.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public List<List<Integer>> getFactors(int n) {
+        return getFactors(n, 2);
+    }
+    
+    public List<List<Integer>> getFactors(int n, int start) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(n <= 1)
+            return ans;
+        for(int i = start; i*i <= n; i++){
+            if(n%i == 0){
+                List<Integer> newList = new ArrayList<>();
+                newList.add(i);
+                newList.add(n/i);
+                ans.add(newList);
+                List<List<Integer>> subAns = getFactors(n/i, i);
+                for(List<Integer> list: subAns){
+                    newList = new ArrayList<>();
+                    newList.add(i);
+                    newList.addAll(list);
+                    ans.add(newList);
+                }
+                
+            }
+        }
+        
+        // if(ans.size() == 0 && start != n){
+        //     List<Integer> newList = new ArrayList<>();
+        //     newList.add(n);
+        //     ans.add(newList);
+        // }
+        
+        return ans;
+    }
+}
