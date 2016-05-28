d653b613aa78ecfe1221ac1c7ce3c70382d00c06
Dong <jellylidong@users.noreply.github.com>
2016-05-28 16:08:43 -0400

    [59. Spiral Matrix II][Accepted]committed by LeetCode Extension

--- 59-Spiral-Matrix-II/solution.java
+++ 59-Spiral-Matrix-II/solution.java
@@ -14,23 +14,26 @@ public class Solution {
             for(int i = left; count <= n*n && i <= right; i++)
                 ans[up][i] = count++;
             up++; //1
-            
+            if(count > n*n) break;
             //go down
             // System.out.println(left + " " + right + " " + up + " " + down);
             for(int i = up; count <= n*n && i <= down; i++)
                 ans[i][right] = count++;
             right--; 
-            
+            if(count > n*n) break;
             //go left
             // System.out.println(left + " " + right + " " + up + " " + down);
             for(int i = right; count <= n*n && i >= left; i--)
                 ans[down][i] = count++;
             down--;
+            if(count > n*n) break;
             
+            //go up
             // System.out.println(left + " " + right + " " + up + " " + down);
-            for(int i = down; count <= n*n && i <= up; i--)
+            for(int i = down; count <= n*n && i >= up; i--)
                 ans[i][left] = count++;
             left++;
+            if(count > n*n) break;
         }
         
         return ans;
