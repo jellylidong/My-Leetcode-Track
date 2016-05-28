3327cb633bd299339f0e8408612fd91db3dbbda8
Dong <jellylidong@users.noreply.github.com>
2016-05-27 23:53:28 -0400

    [46. Permutations][Accepted]committed by LeetCode Extension

--- 46-Permutations/solution.java
+++ 46-Permutations/solution.java
@@ -8,6 +8,12 @@ public class Solution {
         list.add(nums[0]);
         ans.add(list);
         
+        //1st number, 1 position: 1
+        //2nd number, 2 position: 21, 12
+        //3rd, 3 pos: 321, 231, 213, 312, 132, 123
+        //so the method is insert ith number to i-1th results
+        //for each list in i-1the result, insert nums[i] to position 0 to list.size()
+        //note, we need insert at both 0 and list.size(), ie head and tail
         for(int i = 1; i < nums.length; i++){
             List<List<Integer>> newAns = new ArrayList<>();
             for(List<Integer> curList: ans){
