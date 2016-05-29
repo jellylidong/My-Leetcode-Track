ad404297a0f7fb02edcf03356fb574b33f2d8561
Dong <jellylidong@users.noreply.github.com>
2016-05-29 11:00:42 -0400

    [313. Super Ugly Number][Accepted]committed by LeetCode Extension

--- 313-Super-Ugly-Number/solution.java
+++ 313-Super-Ugly-Number/solution.java
@@ -1,13 +1,13 @@
 public class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
         int[] ugly = new int[n];
-        int[] id = new int[n];
+        int[] id = new int[primes.length];
         ugly[0] = 1;
         
         for(int i = 1; i < n; i++){
             int min = Integer.MAX_VALUE;
             for(int j = 0; j < primes.length; j++){
-                min = Math.min(min, ugly[i-1]*primes[j]);
+                min = Math.min(min, ugly[id[j]]*primes[j]);
             }
             
             ugly[i] = min;
