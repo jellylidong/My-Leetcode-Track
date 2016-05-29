c5b43689de8dfdbfce002ea6b7c53251a373b45d
Dong <jellylidong@users.noreply.github.com>
2016-05-29 10:58:38 -0400

    [313. Super Ugly Number][Runtime Error]committed by LeetCode Extension

---
+++ 313-Super-Ugly-Number/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        int[] ugly = new int[n];
+        int[] id = new int[n];
+        ugly[0] = 1;
+        
+        for(int i = 1; i < n; i++){
+            int min = Integer.MAX_VALUE;
+            for(int j = 0; j < primes.length; j++){
+                min = Math.min(min, ugly[i-1]*primes[j]);
+            }
+            
+            ugly[i] = min;
+            
+            for(int j = 0; j < primes.length; j++){
+                if(primes[j]*ugly[id[j]] <= ugly[i])
+                    id[j]++;
+            }
+        }
+        
+        return ugly[n-1];
+    }
+}
