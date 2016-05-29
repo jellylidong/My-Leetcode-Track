83a1857b0d33df8dc8b2c52209682b235b724a0a
Dong <jellylidong@users.noreply.github.com>
2016-05-28 23:10:16 -0400

    [75. Sort Colors][Accepted]committed by LeetCode Extension

--- 75-Sort-Colors/solution.java
+++ 75-Sort-Colors/solution.java
@@ -1,25 +1,50 @@
 public class Solution {
+    // public void sortColors(int[] nums) {
+    //     int red = 0;
+    //     int white = 0;
+    //     int blue = 0;
+    //     for(int i = 0; i < nums.length; i++){
+    //         int n = nums[i];
+    //         if(n == 0)
+    //             red++;
+    //         else if(n == 1)
+    //             white++;
+    //         else
+    //             blue++;
+    //     }
+        
+    //     for(int i = 0; i < nums.length; i++){
+    //         if(i < red)
+    //             nums[i] = 0;
+    //         else if(i < red+white)
+    //             nums[i] = 1;
+    //         else
+    //             nums[i] = 2;
+    //     }
+    // }
+    
+    //started because fail follow up 
     public void sortColors(int[] nums) {
-        int red = 0;
-        int white = 0;
-        int blue = 0;
-        for(int i = 0; i < nums.length; i++){
-            int n = nums[i];
-            if(n == 0)
-                red++;
-            else if(n == 1)
-                white++;
-            else
-                blue++;
+        int lo = 0;
+        int hi = nums.length-1;
+        
+        for(int i = 0; i <= hi; i++){
+            while(nums[i] == 2 && i < hi){
+                swap(nums, i, hi);
+                hi--;
+            }
+            while(nums[i] == 0 && i > lo){
+                swap(nums, i, lo);
+                lo++;
+            }
+                
         }
         
-        for(int i = 0; i < nums.length; i++){
-            if(i < red)
-                nums[i] = 0;
-            else if(i < red+white)
-                nums[i] = 1;
-            else
-                nums[i] = 2;
-        }
+    }
+    
+    public void swap(int[] nums, int i, int j){
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
