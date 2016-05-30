e3be4a48bc13b25a8d038989b71c483098caaa04
Dong <jellylidong@users.noreply.github.com>
2016-05-30 18:38:47 -0400

    [162. Find Peak Element][Accepted]committed by LeetCode Extension

--- 162-Find-Peak-Element/solution.java
+++ 162-Find-Peak-Element/solution.java
@@ -1,20 +1,38 @@
 public class Solution {
     public int findPeakElement(int[] nums) {
-        if(nums.length == 0)
-            return -1;
-        if(nums.length == 1)
-            return 0;
+        // if(nums.length == 0)
+        //     return -1;
+        // if(nums.length == 1)
+        //     return 0;
             
-        for(int i = 1; i < nums.length-1; i++){
-            if(nums[i] > nums[i-1] && nums[i] > nums[i+1])
-                return i;
+        // for(int i = 1; i < nums.length-1; i++){
+        //     if(nums[i] > nums[i-1] && nums[i] > nums[i+1])
+        //         return i;
+        // }
+        
+        // if(nums[0] > nums[1])
+        //     return 0;
+        // if(nums[nums.length-1] > nums[nums.length-2])
+        //     return nums.length-1;
+            
+        // return -1;
+        
+        //above code works but uses O(n)
+        //the problem actually requires us to find a local max value
+        //so we can use binary search to find a local max, which is O(lgn)
+        
+        int lo = 0;
+        int hi = nums.length-1;
+        
+        while(lo < hi){
+            int mid1 = lo + (hi-lo)/2;
+            int mid2 = mid1 + 1;
+            if(nums[mid1] > nums[mid2])
+                hi = mid1;
+            else
+                lo = mid2;
         }
         
-        if(nums[0] > nums[1])
-            return 0;
-        if(nums[nums.length-1] > nums[nums.length-2])
-            return nums.length-1;
-            
-        return -1;
+        return lo;
     }
 }
