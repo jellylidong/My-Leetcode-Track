76c0bdfd3141f2446d5d62bbe633d36200282600
Dong <jellylidong@users.noreply.github.com>
2016-05-30 12:02:34 -0400

    [279. Perfect Squares][Runtime Error]committed by LeetCode Extension

--- 279-Perfect-Squares/solution.java
+++ 279-Perfect-Squares/solution.java
@@ -1,13 +1,18 @@
 public class Solution {
+    HashMap<Integer, Integer> map = new HashMap<>();
     public int numSquares(int n) {
         if(n <= 3)
             return n;
+        
+        if(map.containsKey(n))
+            return map.get(n);
             
         int min = Integer.MAX_VALUE;    
         for(int i = 1; i*i <= n; i++){
             min = Math.min(min, numSquares(n-i*i));
         }
         
+        map.put(n, 1+min);
         return 1 + min;
     }
 }
