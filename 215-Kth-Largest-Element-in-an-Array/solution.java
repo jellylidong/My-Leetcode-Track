597689321812d427d36aeeb09e1c3e2dd4ea4439
Dong <jellylidong@users.noreply.github.com>
2016-05-30 11:14:40 -0400

    [215. Kth Largest Element in an Array][Accepted]committed by LeetCode Extension

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -1,17 +1,19 @@
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k);
         int len = nums.length;
-        if(k >= len)
-            return nums[0];
+        
             
         for(int i = 0; i < len; i++){
-            pq.offer(i);
-            if(pq.size() == k)
+            pq.offer(nums[i]);
+            if(pq.size() > k)
                 pq.poll();
                 
         }
-        
+        // System.out.println(pq.size());
+        // System.out.println(pq.size());
+        // System.out.println(pq.size());
+        // System.out.println(pq.size());
         return pq.peek();
     }
 }
