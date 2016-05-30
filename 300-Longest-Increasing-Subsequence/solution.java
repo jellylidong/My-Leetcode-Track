81a52ad5a370bf22d7891eee4ac0020e2275a885
Dong <jellylidong@users.noreply.github.com>
2016-05-29 21:36:25 -0400

    [300. Longest Increasing Subsequence][Runtime Error]committed by LeetCode Extension

--- 300-Longest-Increasing-Subsequence/solution.java
+++ 300-Longest-Increasing-Subsequence/solution.java
@@ -1,31 +1,52 @@
 public class Solution {
+    
+    //O(n^2)
+    // public int lengthOfLIS(int[] nums) {
+    //     int len = nums.length;
+    //     if(len == 0)
+    //         return 0;
+            
+    //     //count[i] is the max length of suquence end with nums[i], inclusive    
+    //     int[] count = new int[len];
+    //     Arrays.fill(count, 1);
+        
+    //     int lo = 0;
+    //     int hi = 0;
+        
+    //     while(hi < len){
+    //         while(lo < hi){
+    //             if(nums[lo] < nums[hi]){
+    //                 count[hi] = Math.max(count[lo]+1, count[hi]);
+    //             }
+    //             lo++;
+    //         }
+    //         lo = 0;
+    //         hi++;
+    //     }
+        
+    //     int ans = 0;
+    //     for(int n: count)
+    //         ans = Math.max(ans, n);
+            
+    //     return ans;
+    // }
+    
+    
+    //O(nlgn)
     public int lengthOfLIS(int[] nums) {
         int len = nums.length;
         if(len == 0)
             return 0;
-            
-        //count[i] is the max length of suquence end with nums[i], inclusive    
-        int[] count = new int[len];
-        Arrays.fill(count, 1);
-        
-        int lo = 0;
-        int hi = 0;
-        
-        while(hi < len){
-            while(lo < hi){
-                if(nums[lo] < nums[hi]){
-                    count[hi] = Math.max(count[lo]+1, count[hi]);
-                }
-                lo++;
-            }
-            lo = 0;
-            hi++;
+        int[] dp = new int[len];
+        for(int n: nums){
+            int i = Arrays.binarySearch(dp, 0, len, n);
+            if(i < 0)
+                i = -(i+1);
+            dp[i] = n;
+            if(i == len)
+                len++;
         }
         
-        int ans = 0;
-        for(int n: count)
-            ans = Math.max(ans, n);
-            
-        return ans;
+        return len;
     }
 }
