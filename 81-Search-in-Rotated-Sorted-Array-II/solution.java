40259ae0435adca71e4accaf4ce74ecfb5d2d411
Dong <jellylidong@users.noreply.github.com>
2016-05-30 23:18:14 -0400

    [81. Search in Rotated Sorted Array II][Runtime Error]committed by LeetCode Extension

--- 81-Search-in-Rotated-Sorted-Array-II/solution.java
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.java
@@ -5,10 +5,26 @@ public class Solution {
         
         while(lo < hi){
             int mid = lo + (hi-lo)/2;
-            if(nums[mid] > target)
-                hi = mid-1;
-            else
-                lo = mid;
+            if(nums[mid] == target)
+                return true;
+                
+            //pass by all the dup at hi
+            if(nums[mid] < nums[hi]){
+                if(nums[mid] < target && target <= nums[hi])
+                    hi = mid;
+                else
+                    lo = mid+1;
+            }
+            else if(nums[mid] == nums[hi]){
+                hi--;
+            }
+            else{//nums[mid] > nums[hi]
+                if(nums[lo] <= target && target < nums[mid])
+                    hi = mid;
+                else
+                    lo = mid-1;
+            }
+            
         }
         
         return nums[lo] == target;
