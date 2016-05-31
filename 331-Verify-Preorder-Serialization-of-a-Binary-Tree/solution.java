5e14c8c09dbf522cf62a11ba8be2631d81b7852a
Dong <jellylidong@users.noreply.github.com>
2016-05-31 16:17:04 -0400

    [331. Verify Preorder Serialization of a Binary Tree][Accepted]committed by LeetCode Extension

--- 331-Verify-Preorder-Serialization-of-a-Binary-Tree/solution.java
+++ 331-Verify-Preorder-Serialization-of-a-Binary-Tree/solution.java
@@ -42,28 +42,52 @@ public class Solution {
     // }
     
     
+    //!!!!!!!!!!!!!!!!!!!
     //the above code can not handle some cases with 3#, ie ...###...
-    public boolean isValidSerialization(String preorder) {
+    // public boolean isValidSerialization(String preorder) {
+    //     String[] ss = preorder.split(",");
+        
+    //     //we keep track of the diff of out and in degrees
+    //     //for every new node, diff-- because each node must have an in degree
+    //     //after diff--, if diff < 0, then return false because out should always >= in
+    //     //then if this node is not #, out += 2
+        
+    //     //another trick is initialize diff = 1 so that it can also handle the case for root node(root node has 0 in degree)
+    //     //finally, diff should be 0 for a correct inorder travesal
+        
+    //     int diff = 1;
+    //     for(String s: ss){
+    //         diff--;
+    //         if(diff < 0)
+    //             return false;
+    //         if(!s.equals("#"))
+    //             diff += 2;
+    //     }
+        
+    //     return diff == 0;
+    // }
+    
+    
+    //another method, stack
+     public boolean isValidSerialization(String preorder) {
+        Stack<String> stk = new Stack<>();
         String[] ss = preorder.split(",");
         
-        //we keep track of the diff of out and in degrees
-        //for every new node, diff-- because each node must have an in degree
-        //after diff--, if diff < 0, then return false because out should always >= in
-        //then if this node is not #, out += 2
-        
-        //another trick is initialize diff = 1 so that it can also handle the case for root node(root node has 0 in degree)
-        //finally, diff should be 0 for a correct inorder travesal
-        
-        int diff = 1;
-        for(String s: ss){
-            diff--;
-            if(diff < 0)
-                return false;
-            if(!s.equals("#"))
-                diff += 2;
+        for(int i = 0; i < ss.length; i++){
+            if(ss[i].equals("#")){
+                while(!stk.isEmpty() && stk.peek().equals("#")){
+                    stk.pop();
+                    if(stk.isEmpty())
+                        return false;
+                    stk.pop();
+                }
+                stk.push("#");
+            }
+            else
+                stk.push(ss[i]);
         }
         
-        return diff == 0;
-    }
+        return stk.size() == 1 && stk.peek().equals("#");
+     }
     
 }
