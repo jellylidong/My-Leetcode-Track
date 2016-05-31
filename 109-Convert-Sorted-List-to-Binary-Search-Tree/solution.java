d1918de91ae93816340f3532e3774dccde2ec736
Dong <jellylidong@users.noreply.github.com>
2016-05-31 18:14:39 -0400

    [109. Convert Sorted List to Binary Search Tree][Accepted]committed by LeetCode Extension

---
+++ 109-Convert-Sorted-List-to-Binary-Search-Tree/solution.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    ListNode node;
+    public TreeNode sortedListToBST(ListNode head) {
+        node = null;
+        int len = 0;
+        ListNode cur = head;
+        while(cur != null){
+            len++;
+            cur = cur.next;
+        }
+        
+        node = head;
+        return helper(0, len-1);
+    }
+    
+    public TreeNode helper(int lo, int hi){
+        if(lo > hi)
+            return null;
+        
+        int mid = lo + (hi-lo)/2;
+        TreeNode left = helper(lo, mid-1);
+        TreeNode root = new TreeNode(node.val);
+        
+        node = node.next;
+        TreeNode right = helper(mid+1, hi);
+        
+        root.left = left;
+        root.right = right;
+        
+        return root;
+        
+    }
+}
