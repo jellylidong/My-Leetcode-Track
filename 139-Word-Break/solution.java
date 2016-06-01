01f8acedfe652a88aa71796c9538958f830efc55
Dong <jellylidong@users.noreply.github.com>
2016-06-01 03:15:08 -0400

    [139. Word Break][Accepted]committed by LeetCode Extension

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -1,34 +1,31 @@
 public class Solution {
+    
+    /*
+    Thought: compared to other sumbissions (which is wrong somehow but the method should be correct, but can't find why)
+    In this method, instead of using dfs to find the deeper result and then compute the shallower one with the deeper one
+    We compute from shallow to deep
+    This is exactly DP
+    So when DFS is complex to implement
+    Consider forward DP (from shallow to deep)
+    */
     public boolean wordBreak(String s, Set<String> dict) {
-    
+        int len = s.length();
+        boolean[] res = new boolean[len+1];
+        //res[i] is true if s.substring(0, i) is breakable
+        res[0] = true;
         
-        HashMap<Integer, Boolean> map = new HashMap<>();
-        return wordBreak(s, dict, 0, map);
-    }
-    
-    public boolean wordBreak(String s, Set<String> dict, int start, HashMap<Integer, Boolean> map){
-        for(int i = start; i <= s.length(); i++){
-            if(map.containsKey(i))
-                return map.get(i);
-            else if (dict.contains(s.substring(start, i))){
-                if(i == s.length()){
-                    map.put(start, true);
-                    return true;
-                }
-                else{
-                    boolean cur = wordBreak(s, dict, i, map);
-                    if(cur == true){
-                        map.put(start, true);
-                        return true;
-                    }
-                    // else{
-                    //     map.put(start, false);
-                    // }
-                }
+        for(int i = 1; i <= s.length(); i++){
+            for(int j = 0; j < i; j++){
+                res[i] = res[j] && dict.contains(s.substring(j, i));
+                if(res[i])
+                    break;
             }
         }
         
-        map.put(start, false);
-        return false;
+        return res[len];
+        
+        
     }
+    
+    
 }
