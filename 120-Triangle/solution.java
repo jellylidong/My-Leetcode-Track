f05b31ad03d87c29a62be404625b51126039b249
Dong <jellylidong@users.noreply.github.com>
2016-05-31 23:45:29 -0400

    [120. Triangle][Accepted]committed by LeetCode Extension

--- 120-Triangle/solution.java
+++ 120-Triangle/solution.java
@@ -1,21 +1,28 @@
 public class Solution {
-    int ans;
+    
     public int minimumTotal(List<List<Integer>> tri) {
-        ans = Integer.MAX_VALUE;
-        minSum(tri, 0, 0, 0);
+        int ans = Integer.MAX_VALUE;
+        
+        for(int i = 1; i <tri.size(); i++){
+            
+            List<Integer> cur = tri.get(i);
+            for(int j = cur.size()-1; j >= 0; j--){
+                if(j == cur.size() - 1)
+                    cur.set(j, cur.get(j) + tri.get(i-1).get(j-1));
+                else if(j == 0)
+                    cur.set(j, cur.get(j) + tri.get(i-1).get(j));
+                else{
+                    cur.set(j, cur.get(j) + Math.min(tri.get(i-1).get(j), tri.get(i-1).get(j-1)));
+                }
+            }
+        }
+        
+        for(int n: tri.get(tri.size()-1)){
+            ans = Math.min(ans, n);
+        }
+        
         return ans;
     }
     
-    public void minSum(List<List<Integer>> tri, int level, int pp, int sum){
-        if(level == tri.size()){
-            ans = Math.min(ans, sum);
-            return;
-        }
-            
-        // if(pp - 1 >= 0)
-        //     minSum(tri, level+1, pp-1, sum + tri.get(level).get(pp-1));
-        minSum(tri, level+1, pp, sum + tri.get(level).get(pp));
-        if(pp+1 < tri.get(level).size())
-            minSum(tri, level+1, pp+1, sum + tri.get(level).get(pp+1));
-    }
+    
 }
