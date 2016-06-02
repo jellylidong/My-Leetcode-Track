a37bdcfc386d446dc091cc0a70285c61f87a4057
Dong <jellylidong@users.noreply.github.com>
2016-06-01 23:37:18 -0400

    [55. Jump Game][Accepted]committed by LeetCode Extension

--- 55-Jump-Game/solution.java
+++ 55-Jump-Game/solution.java
@@ -3,6 +3,8 @@ public class Solution {
         int max = 0 + nums[0];
         for(int i = 1; i <= max && i < nums.length; i++){
             max = Math.max(max, i+nums[i]);
+            if(max >= nums.length-1)
+                return true;
         }
             
         return max >= nums.length-1;
