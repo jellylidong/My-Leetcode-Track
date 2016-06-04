062fc42643756c15e315a80635cab159b3358dff
Dong <jellylidong@users.noreply.github.com>
2016-06-04 14:59:01 -0400

    [69. Sqrt(x)][Accepted]committed by LeetCode Extension

--- 69-Sqrt(x)/solution.java
+++ 69-Sqrt(x)/solution.java
@@ -3,6 +3,8 @@ public class Solution {
         
         if(x <= 0)
             return 0;
+            
+        //for math problems, we must consider edge values like Integer.MAX_VALUE and Integer.MIN_VALUE
         long lo = 1; 
         long hi = x/2;
         
@@ -11,6 +13,9 @@ public class Solution {
             long mid = lo + (hi-lo)/2;
             if(mid*mid == x)
                 return (int)mid;
+            
+            //different from traditional binary search, when we change boundary
+            //we have to check (mid+1)^2 to make we won't go out of the target range
             else if(mid*mid < x){
                 if((mid+1)*(mid+1) > x)
                     return (int)mid;
