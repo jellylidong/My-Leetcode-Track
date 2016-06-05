e9d0e78edb7bdf3ed89211e957dafa790429762b
Dong <jellylidong@users.noreply.github.com>
2016-06-05 00:04:39 -0400

    [61. Rotate List][Runtime Error]committed by LeetCode Extension

---
+++ 61-Rotate-List/solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        int len = 0;
+        ListNode cur = head;
+        while(cur != null){
+            cur = cur.next;
+            len++;
+        }
+        if(len == 0)
+            return null;
+        k = k%len;
+        ListNode pre = head;
+        cur = head;
+        for(int i = 0; i < len-k; i++){
+            pre = cur;
+            cur = cur.next;
+        }
+        pre.next = null;
+        ListNode newHead = cur;
+        pre = cur;
+        while(cur != null){
+            pre = cur;
+            cur = cur.next;
+        }
+        pre.next = head;
+        return newHead;
+    }
+}
