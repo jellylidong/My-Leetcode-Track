d25dc010ba7aa79a9d2a8816755b49c7d698712c
Dong <jellylidong@users.noreply.github.com>
2016-06-05 13:14:00 -0400

    [98. Validate Binary Search Tree][Accepted]committed by LeetCode Extension

---
+++ 98-Validate-Binary-Search-Tree/solution.java
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    
+    //inorder traverse but only keep the last one we traversed
+    //a valid BST should alway increase when inorder traverse,
+    //so if current node <= last one, return false
+    
+    // int pre = Integer.MIN_VALUE;
+    
+    // public boolean isValidBST(TreeNode root) {
+    //     if(root == null)
+    //         return true;
+            
+    //     if(root.left == null && root.right == null){
+    //         if(root.val <= pre)
+    //             return false;
+                
+    //         pre = root.val;
+    //         return true;
+    //     }
+    //     boolean left = isValidBST(root.left);
+    //     if(root.val <= pre)
+    //         return false;
+    //     else
+    //         pre = root.val;
+    //     boolean right = isValidBST(root.right);
+        
+    //     return left && right;
+    // }
+    
+    
+    //above code can not handle case [-2147483648] when we set the pre value int_min
+    //to solve this, this a TreeNode instead
+    TreeNode pre = null;
+    
+    public boolean isValidBST(TreeNode root) {
+        if(root == null)
+            return true;
+            
+        if(root.left == null && root.right == null){
+            
+            if(pre != null && root.val <= pre.val)
+                return false;
+                
+            pre = root;
+            return true;
+        }
+        boolean left = isValidBST(root.left);
+        if(pre != null && root.val <= pre.val)
+            return false;
+        else
+            pre = root;
+        boolean right = isValidBST(root.right);
+        
+        return left && right;
+    }
+    
+    
+    
+    
+    // [3,1,5,0,2,4,6,null,null,null,3] 
+    // [1,null,1]
+    // [-2147483648] 
+    // when use max or min value, we should consider if the value can exist in the question, 
+    // ie min value can be a node value
+}
