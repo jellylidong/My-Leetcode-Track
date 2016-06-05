8da6f454423003ecd388d7b05f18e36a5bbaaa2e
Dong <jellylidong@users.noreply.github.com>
2016-06-05 17:28:09 -0400

    [45. Jump Game II][Accepted]committed by LeetCode Extension

---
+++ 45-Jump-Game-II/solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int jump(int[] nums) {
+        if(nums.length < 2)
+            return 0;
+        
+        int curMax = 0;
+        int nextMax = 0;
+        int step = 0;
+        
+        int i = 0; // current position
+        while(nums.length-i+1 > 0){
+            step++;
+            for(; i <= curMax; i++){
+                nextMax = Math.max(nextMax, nums[i]+i);
+                if(nextMax >= nums.length-1)
+                    return step;
+            }
+            curMax = nextMax;
+        }
+        
+        return -1;
+    }
+    
+    // [7,0,9,6,9,6,1,7,9,0,1,2,9,0,3]
+}
