c43b3682eb8886bfc3a213e17c63e8ba8437f8f4
Dong <jellylidong@users.noreply.github.com>
2016-06-04 22:17:23 -0400

    [148. Sort List][Accepted]committed by LeetCode Extension

---
+++ 148-Sort-List/solution.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null)
+            return null;
+        if(head != null && head.next == null)
+            return head;
+            
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode pre  = head;
+        
+        while(fast != null){
+            pre = slow;
+            slow = slow.next;
+            
+            fast = fast.next;
+            if(fast != null)
+                fast = fast.next;
+        }
+        pre.next = null;
+        
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+        
+        return merge(l1, l2);
+    }
+    
+    public ListNode merge(ListNode l1, ListNode l2){
+        ListNode helper = new ListNode(0);
+        ListNode pre = helper;
+        
+        while(l1 != null || l2 != null){
+            if(l1 != null && l2 != null){
+                if(l1.val < l2.val){
+                    pre.next =l1;
+                    pre = pre.next;
+                    l1 = l1.next;
+                }
+                else{
+                    pre.next = l2;
+                    pre = pre.next;
+                    l2 = l2.next;
+                }
+            }
+            else{
+                while(l1 != null){
+                    pre.next = l1;
+                    pre = pre.next;
+                    l1 = l1.next;
+                }
+                while(l2 != null){
+                    pre.next= l2;
+                    pre = pre.next;
+                    l2 = l2.next;
+                }
+            }
+        }
+        
+        return helper.next;
+    }
+}
