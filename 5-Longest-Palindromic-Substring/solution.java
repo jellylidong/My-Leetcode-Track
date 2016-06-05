dc844f5e5f4764a89fbda3d6564bfbc5c462f2c7
Dong <jellylidong@users.noreply.github.com>
2016-06-05 15:40:21 -0400

    [5. Longest Palindromic Substring][Accepted]committed by LeetCode Extension

--- 5-Longest-Palindromic-Substring/solution.java
+++ 5-Longest-Palindromic-Substring/solution.java
@@ -3,22 +3,29 @@ public class Solution {
         int max = 0;
         String ans = "";
         
+        int lo = 0;
+        int hi = 0;
+        
         for(int i = 0; i < s.length(); i++){
             int n1 = search(s.substring(0, i), s.substring(i+1)) + 1;
             int n2 = search(s.substring(0, i+1), s.substring(i+1));
             if(n1 > n2 && n1 > max){
                 max = n1;
                 int half = (n1-1)/2;
-                ans = s.substring(i-half, i) + s.charAt(i) + s.substring(i+1, i+1+half);
+                // ans = s.substring(i-half, i) + s.charAt(i) + s.substring(i+1, i+1+half);
+                lo = i-half;
+                hi = i+1+half;
             }
             if(n2 > n1 && n2 > max){
                 max = n2;
                 int half = n2/2;
-                ans = s.substring(i+1-half, i+1) + s.substring(i+1, i+1+half);
+                // ans = s.substring(i+1-half, i+1) + s.substring(i+1, i+1+half);
+                lo = i+1-half;
+                hi = i+1+half;
             }
         }
         
-        return ans;
+        return s.substring(lo, hi);
     }
     
     public int search(String s1, String s2){
