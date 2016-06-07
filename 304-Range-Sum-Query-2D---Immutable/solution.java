4a1e574c09a669d2115deb905b8d4b9dd38b47ba
Dong <jellylidong@users.noreply.github.com>
2016-06-07 11:34:10 -0400

    [304. Range Sum Query 2D - Immutable][Time Limit Exceeded]committed by LeetCode Extension

--- 304-Range-Sum-Query-2D---Immutable/solution.java
+++ 304-Range-Sum-Query-2D---Immutable/solution.java
@@ -13,7 +13,7 @@ public class NumMatrix {
         for(int i = 1; i < w; i++)
             sum[0][i] = m[0][i-1] + m[0][i];
         for(int i = 1; i < h; i++)
-            sum[0][i] = sum[0][i-1] + m[i][0];
+            sum[i][0] = sum[i-1][0] + m[i][0];
             
         for(int i = 1; i < h; i++){
             for(int j = 1; j < w; j++)
