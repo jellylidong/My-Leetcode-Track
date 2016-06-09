656222ff9f37432c5b135849d6a77b86f0c713d3
Dong <jellylidong@users.noreply.github.com>
2016-06-09 15:17:53 -0400

    [115. Distinct Subsequences][Accepted]committed by LeetCode Extension

--- 115-Distinct-Subsequences/solution.java
+++ 115-Distinct-Subsequences/solution.java
@@ -16,7 +16,7 @@ public class Solution {
         
         for(int i = 1; i <= lt; i++){
             for(int j = 1; j <= ls; j++){
-                if(t.charAt(i) == s.charAt(j)){
+                if(t.charAt(i-1) == s.charAt(j-1)){
                     
                     dp[i][j] = dp[i][j-1] + dp[i-1][j-1];
                 }
