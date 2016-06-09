e041a1d1020919051e912288552a8c8fd9c7bca8
Dong <jellylidong@users.noreply.github.com>
2016-06-09 02:42:35 -0400

    [340. Longest Substring with At Most K Distinct Characters][Accepted]committed by LeetCode Extension

--- 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.java
+++ 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.java
@@ -1,4 +1,11 @@
 public class Solution {
+    
+    //this problem is easy to think but when operating, 
+    //there are many small tips that can make the code run to null pointer exceptions
+    
+    //the basic iead is we use a hash map to keep the char and its index
+    //if the map size > k, we need to remove the first removable char from the left of current Longest Substring with At Most K Distinct Characters 
+    
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
         if(k == 0)
             return 0;
@@ -10,17 +17,21 @@ public class Solution {
         int len = 0;
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
-            // System.out.println(c + " " + i);
+
             map.put(c, i);
-            // System.out.println(map.size());
+     
             if(map.size() > k){
-                char c2 = s.charAt(start+1);
-                // System.out.println(c2);
-                int start2 = map.get(c2);
-                // System.out.println("start 2:" + start2);
+                char c2 = s.charAt(start+1); // the first char of current Longest Substring with k+1 Distinct Characters 
+     
+                int start2 = map.get(c2); // the right most position of this char 
+                //this means we only need to remove one of the char between start+1 and start2, 
+                //(note start start from 0, ie start is the previous one positon of the current substring)
+                //then we can keep the hash map size to k
+                
                 int j = start+1;
                 for(; j <= start2 && map.size() > k; j++){
-                    if(map.containsKey(s.charAt(j)) && j == map.get(s.charAt(j)))
+                    //this judge is very important
+                    if(j == map.get(s.charAt(j)))
                         map.remove(s.charAt(j));
                 }
                 start = j-1;
