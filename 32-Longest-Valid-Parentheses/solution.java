d46336fde722193a67f9ad97a355cc45e58a780a
Dong <jellylidong@users.noreply.github.com>
2016-06-09 13:16:47 -0400

    [32. Longest Valid Parentheses][Runtime Error]committed by LeetCode Extension

--- 32-Longest-Valid-Parentheses/solution.java
+++ 32-Longest-Valid-Parentheses/solution.java
@@ -1,25 +1,47 @@
 public class Solution {
+    // public int longestValidParentheses(String s) {
+    //     int max = 0;
+    //     char[] cs = s.toCharArray();
+    //     Stack<Integer> stk = new Stack<>();
+    //     for(int i = 0; i < s.length(); i++){
+    //         char c = cs[i];
+    //         if(c == '(')
+    //             stk.push(i);
+    //         else if(c == ')'){
+    //             if(stk.isEmpty() || s.charAt(stk.peek()) == ')'){
+    //                 // max = Math.min(max, i);
+    //                 stk.push(i);
+    //             }
+    //             else if(s.charAt(stk.peek()) == '('){
+    //                 stk.pop();
+    //                 if(stk.isEmpty())
+    //                     max = Math.max(max, i);
+    //                 else
+    //                     max = Math.max(max, i-stk.peek());
+    //             }
+    //         }
+    //     }
+        
+    //     return max;
+    // }
+    
+    
+    //above code get TLE for case "((((.....((((" (all chars are "(" and very long)
     public int longestValidParentheses(String s) {
+        int[] dp = new int[s.length()]; 
         int max = 0;
-        char[] cs = s.toCharArray();
-        Stack<Integer> stk = new Stack<>();
-        for(int i = 0; i < s.length(); i++){
-            char c = cs[i];
+        for(int i = 1; i < s.length(); i++){
+            char c = s.charAt(i);
             if(c == '(')
-                stk.push(i);
-            else if(c == ')'){
-                if(stk.isEmpty() || s.charAt(stk.peek()) == ')'){
-                    // max = Math.min(max, i);
-                    stk.push(i);
-                }
-                else if(s.charAt(stk.peek()) == '('){
-                    stk.pop();
-                    if(stk.isEmpty())
-                        max = Math.max(max, i);
-                    else
-                        max = Math.max(max, i-stk.peek());
-                }
+                continue;
+            if(c == ')'){
+                if(s.charAt(i-1) == '(')
+                    dp[i] = i-2 >= 0? dp[i-2] + 2: 2;
+                if(s.charAt(i-1) == ')')
+                    dp[i] = dp[i-1-dp[i-1]] + 2;
             }
+            
+            max = Math.max(max, dp[i]);
         }
         
         return max;
