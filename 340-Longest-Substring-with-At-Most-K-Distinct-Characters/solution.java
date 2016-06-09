0a8851f483ca534a446a3e3f8b4bc3bd9e60d83a
Dong <jellylidong@users.noreply.github.com>
2016-06-09 01:33:48 -0400

    [340. Longest Substring with At Most K Distinct Characters][Runtime Error]committed by LeetCode Extension

--- 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.java
+++ 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.java
@@ -10,9 +10,10 @@ public class Solution {
         int len = 0;
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
+            // System.out.println(c + " " + i);
             map.put(c, i);
             if(map.size() > k){
-                char c2 = s.charAt(i-len+1);
+                char c2 = s.charAt(start+1);
                 int start2 = map.get(c2);
                 for(int j = start+1; j <= start2; j++){
                     map.remove(s.charAt(j));
