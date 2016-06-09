554e7da4388abc4e1657b5f1b4693ed441f04fea
Dong <jellylidong@users.noreply.github.com>
2016-06-09 13:31:25 -0400

    [32. Longest Valid Parentheses][Time Limit Exceeded]committed by LeetCode Extension

--- 32-Longest-Valid-Parentheses/solution.java
+++ 32-Longest-Valid-Parentheses/solution.java
@@ -30,15 +30,22 @@ public class Solution {
     public int longestValidParentheses(String s) {
         int[] dp = new int[s.length()]; 
         int max = 0;
+        char[] cs = s.toCharArray();
         for(int i = 1; i < s.length(); i++){
-            char c = s.charAt(i);
+            char c = cs[i];
             if(c == '(')
                 continue;
             if(c == ')'){
                 if(s.charAt(i-1) == '(')
                     dp[i] = i-2 >= 0? dp[i-2] + 2: 2;
-                if(s.charAt(i-1) == ')')
-                    dp[i] = i-1-dp[i-1] >= 0? dp[i-1-dp[i-1]] + 2:0;
+                if(s.charAt(i-1) == ')'){
+                    if(i-1-dp[i-1]-1 >= 0)
+                        dp[i] = dp[i-1-dp[i-1]-1] + dp[i-1] + 2;
+                    else
+                        dp[i] = 0;
+                    
+                    // dp[i] = i-1-dp[i-1]-1 >= 0? dp[i-1-dp[i-1]-1] + dp[i-1] + 2: dp[i-1] + 2;
+                }
             }
             
             max = Math.max(max, dp[i]);
