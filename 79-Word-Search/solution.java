c79e2f201f8552e8d6b71c501f4179ddbd126e44
Dong <jellylidong@users.noreply.github.com>
2016-06-11 17:39:10 -0400

    [79. Word Search][Accepted]committed by LeetCode Extension

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -9,11 +9,14 @@ public class Solution {
             
         if(word.length() == 0)
             return false;
+        if(word.length() > m*n)
+            return false;
         
         boolean[][] visited = new boolean[m][n];
+        char[] cs = word.toCharArray();
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(search(i, j, visited, board, word, 0))
+                if(search(i, j, visited, board, cs, 0))
                     return true;
             }
         }
@@ -21,23 +24,27 @@ public class Solution {
         return false;
     }
     
-    public boolean search(int i, int j, boolean[][] visited, char[][] board, String word, int p){
-        if(p == word.length())
+    public boolean search(int i, int j, boolean[][] visited, char[][] board, char[] cs, int p){
+        if(p == cs.length)
             return true;
         if(i < 0 || j < 0 || i == board.length || j == board[0].length)
             return false;
         if(visited[i][j])
             return false;
-        if(board[i][j] != word.charAt(p))
+        if(board[i][j] != cs[p])
             return false;
             
         visited[i][j] = true;
-        boolean left = search(i, j-1, visited, board, word, p+1);
-        boolean right = search(i, j+1, visited, board, word, p+1);
-        boolean up = search(i-1, j, visited, board, word, p+1);
-        boolean down = search(i+1, j, visited, board, word, p+1);
+        boolean left = search(i, j-1, visited, board, cs, p+1);
+        if(left)    return true;
+        boolean right = search(i, j+1, visited, board, cs, p+1);
+        if(right)   return true;
+        boolean up = search(i-1, j, visited, board, cs, p+1);
+        if(up)  return true;
+        boolean down = search(i+1, j, visited, board, cs, p+1);
+        if(down)    return true;
         visited[i][j] = false;
         
-        return left || right || up || down;
+        return false;
     }
 }
