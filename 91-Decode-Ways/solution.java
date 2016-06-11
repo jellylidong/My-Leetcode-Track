77ce061e536fcd460ac1dd9b521f65720464e214
Dong <jellylidong@users.noreply.github.com>
2016-06-11 19:16:16 -0400

    [91. Decode Ways][Accepted]committed by LeetCode Extension

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -1,24 +1,47 @@
 public class Solution {
+    //hard point: how to handle 0 and number > 26
+    
     int ans;
     public int numDecodings(String s) {
         ans = 0;
         if(s.length() == 0)
             return ans;
-        search(0, s);
+        int[] dp = new int[s.length()]; //dp[i] means decode ways of s.substring(i);
         
-        return ans;
+        int len = s.length();
+        dp[len-1] = Integer.parseInt(s.substring(len-1, len)) > 0? 1: 0;
+        if(len >= 2){
+            int n = Integer.parseInt(s.substring(len-2));
+            if(n < 10)
+                dp[len-2] = 0;
+            else if(n <= 26)
+                dp[len-2] = 1 + dp[len-1];
+            else  if(n%10 != 0)
+                dp[len-2] = 1;
+            else
+                return 0;
+            
+        }
+        
+        for(int i = len-3; i >= 0; i--){
+            int n = Integer.parseInt(s.substring(i, i+1));
+            if(n == 0)
+                dp[i] = 0;
+            else dp[i] += dp[i+1];
+            n = Integer.parseInt(s.substring(i, i+2));
+            if(n < 10)
+                dp[i] = 0;
+            else if(n <= 26)
+                dp[i] += dp[i+2];
+            else if(n%10 != 0)
+                dp[i] = dp[i+1];
+            else
+                return 0;
+        }
+        
+        return dp[0];
     }
     
-    public void search(int i , String s){
-        if(i == s.length()){
-            ans++;
-            return;
-        }
-        search(i+1, s);
-        if(i+2 <= s.length()){
-            int n = Integer.parseInt(s.substring(i, i+2));
-            if(n <= 26)
-                search(i+2, s);
-        }
-    }
+   // "230" 
+   // "611"
 }
