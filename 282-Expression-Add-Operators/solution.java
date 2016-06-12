f953b001d0f6d2d781930f1125f7396b161dc9e8
Dong <jellylidong@users.noreply.github.com>
2016-06-12 18:33:49 -0400

    [282. Expression Add Operators][Accepted]committed by LeetCode Extension

--- 282-Expression-Add-Operators/solution.java
+++ 282-Expression-Add-Operators/solution.java
@@ -9,13 +9,21 @@ public class Solution {
         return ans;
     }
     
+    //the preNum is used to store the last number we add to our stirng
+    //it will be used for multipy
+    //when we wanted to mult current number
+    //our next curVal should be curVal - preNum + preNum*newNum
     public void helper(String s, int lo, int hi, String curStr, long curVal, long preNum, int target, List<String> ans){
         // if(curStr.length() == 2*s.length()-1 && curVal == target)
         
+        //note can not use curStr.length() == 2*s.length()-1 as a condition for if to add current string
+        //because we may use two chars as a number, then the final length is smaller s*len-1
         if(lo == s.length() && curVal == target)
             ans.add(curStr);
         else{
             for(int i = lo; i <= hi; i++){
+                //except 0, number start with 0 is invalid
+                //can mistake happens usually for string to int problems
                 if(i != lo && s.charAt(lo) == '0')
                     break;
                     
