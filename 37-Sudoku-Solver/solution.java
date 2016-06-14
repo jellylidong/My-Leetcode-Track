bd0d27107f357651a46e952bfb47b886e167edef
Dong <jellylidong@users.noreply.github.com>
2016-06-14 14:56:58 -0400

    [37. Sudoku Solver][Accepted]committed by LeetCode Extension

--- 37-Sudoku-Solver/solution.java
+++ 37-Sudoku-Solver/solution.java
@@ -13,12 +13,23 @@ public class Solution {
             sqr[i] = new HashSet<Character>();
         }
         
+        
+        //if we want to use hashset to store already used numbers, 
+        //we must first store the already used number of intial board first
+        //becuase in later traverse, assume we meet a place need to be filled,
+        //if we don't store the already used values, it's likely that we use a number that is already used in current later positions
         for(int i = 0; i < len; i++){
             for(int j = 0; j < len; j++){
                 char c = board[i][j];
                 if(c != '.' ){
                     row[i].add(c);
                     col[j].add(c);
+                    
+                    //the trick for transform i,j to 9-number sqr set
+                    //note that row 0,1,2 can only be in sqr[0,1,1], row 3,4,5 can only be in sqr[3,4,5]
+                    //so we can first decide their first part by i/3*3
+                    //then for row 1,2,3, which sqr they should be in depends on j, the second part j/3
+                    //so for board[i][j], it shuold be at sqr[i/3*3 + j/3]
                     sqr[i/3*3+j/3].add(c);
                 }
             }
