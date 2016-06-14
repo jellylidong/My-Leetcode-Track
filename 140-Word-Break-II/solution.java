6bc7c67467e1ffb8c61cf3fefdde0db8adcf0f95
Dong <jellylidong@users.noreply.github.com>
2016-06-14 17:20:15 -0400

    [140. Word Break II][Time Limit Exceeded]committed by LeetCode Extension

--- 140-Word-Break-II/solution.java
+++ 140-Word-Break-II/solution.java
@@ -1,14 +1,14 @@
 public class Solution {
     public List<String> wordBreak(String s, Set<String> wordDict) {
         List<String> list = new ArrayList<>();
-        for(int i = s.length()-1; i >= 0; i--){
-            if(wordDict.contains(s.substring(i)))
-                break;
-            else{
-                if(i == 0)
-                    return list;
-            }
-        }
+        // for(int i = s.length()-1; i >= 0; i--){
+        //     if(wordDict.contains(s.substring(i)))
+        //         break;
+        //     else{
+        //         if(i == 0)
+        //             return list;
+        //     }
+        // }
         
         for(int i = 0; i < s.length(); i++){
             if(wordDict.contains(s.substring(0, i))){
