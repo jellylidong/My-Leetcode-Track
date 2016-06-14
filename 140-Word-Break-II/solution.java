a1433a3ffca129172e784dcb222fab216356c7fe
Dong <jellylidong@users.noreply.github.com>
2016-06-14 18:36:43 -0400

    [140. Word Break II][Accepted]committed by LeetCode Extension

--- 140-Word-Break-II/solution.java
+++ 140-Word-Break-II/solution.java
@@ -1,28 +1,28 @@
 public class Solution {
-    public List<String> wordBreak(String s, Set<String> wordDict) {
-        List<String> list = new ArrayList<>();
-        // for(int i = s.length()-1; i >= 0; i--){
-        //     if(wordDict.contains(s.substring(i)))
-        //         break;
-        //     else{
-        //         if(i == 0)
-        //             return list;
-        //     }
-        // }
+    // public List<String> wordBreak(String s, Set<String> wordDict) {
+    //     List<String> list = new ArrayList<>();
+    //     // for(int i = s.length()-1; i >= 0; i--){
+    //     //     if(wordDict.contains(s.substring(i)))
+    //     //         break;
+    //     //     else{
+    //     //         if(i == 0)
+    //     //             return list;
+    //     //     }
+    //     // }
         
-        for(int i = 0; i < s.length(); i++){
-            if(wordDict.contains(s.substring(0, i))){
-                String head = s.substring(0, i);
-                List<String> cur = wordBreak(s.substring(i), wordDict);
-                for(String str: cur){
-                    list.add(head + " " + str);
-                }
-            }
-        }
-        if(wordDict.contains(s))
-            list.add(s);
-        return list;
-    }
+    //     for(int i = 0; i < s.length(); i++){
+    //         if(wordDict.contains(s.substring(0, i))){
+    //             String head = s.substring(0, i);
+    //             List<String> cur = wordBreak(s.substring(i), wordDict);
+    //             for(String str: cur){
+    //                 list.add(head + " " + str);
+    //             }
+    //         }
+    //     }
+    //     if(wordDict.contains(s))
+    //         list.add(s);
+    //     return list;
+    // }
     
     // public List<String> wordBreak(String s, Set<String> wordDict, int max){
         
@@ -39,5 +39,41 @@ public class Solution {
     //then if in another substirng s2, if we find substring at j again, we don't need to recurse into further level
     //we can find it from the map
     //this saves a lot time
+    
+    public List<String> wordBreak(String s, Set<String> dict) {
+        
+            
+        
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        
+        return solve(s, dict, map);
+        
+    }
+    
+    public List<String> solve(String s, Set<String> dict, HashMap<String, ArrayList<String>> map){
+        if(map.containsKey(s))
+            return map.get(s);
+            
+        ArrayList<String> curList = new ArrayList<>();
+        if(dict.contains(s))
+            curList.add(s);
+            
+        for(String str: dict){
+            if(s.startsWith(str)){
+                List<String> nextList = solve(s.substring(str.length()), dict, map);
+                for(String nextStr: nextList)
+                    curList.add(str + " " + nextStr);
+            }
+        }
+        
+        map.put(s, curList);
+        
+        return curList;
+        
+    }
+    
+    
+    
+    
     
 }
