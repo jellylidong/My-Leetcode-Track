c5a7251f8894cbfd5c9e70851789f101a8ef274c
Dong <jellylidong@users.noreply.github.com>
2016-06-15 15:36:38 -0400

    [84. Largest Rectangle in Histogram][Accepted]committed by LeetCode Extension

---
+++ 84-Largest-Rectangle-in-Histogram/solution.java
@@ -0,0 +1,62 @@
+public class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+        for(int i = 0; i < heights.length; i++){
+            if(stack.isEmpty() || heights[i] >= heights[stack.peek()])
+                stack.push(i);
+            else{
+                while(!stack.isEmpty() && heights[i] <= heights[stack.peek()]){
+                    // stack.pop();
+                    //after the while loop, all height > heights[i] is popped
+                    int left = stack.pop();
+                    int len = 0;
+                    if(stack.isEmpty())
+                        len = i - 1 - (-1+1) + 1;
+                    else
+                        len = i - 1 -stack.peek()-1 + 1;
+                    int area = len * heights[left];
+                    max = Math.max(max, area);
+                }
+                //note the area here does not include i
+                //now if stack is empty, it means all heights before i is <= heights[i]
+                //then the length is (i-1) - (-1+1) + 1 = i
+                //if stack is not empty, the peek of stack is heights[stack.peek()] > heights[i]
+                //then the length is (i-1) - (stack.peek()+1) + 1 = i - stack.peek() - 1
+                
+                // int len = 0;
+                // if(stack.isEmpty())
+                //     len = i;
+                // else
+                //     len = i - stack.peek() - 1;
+                // int area = len * heights[i];
+                // max = Math.max(area, max);
+                
+                stack.push(i);
+            }
+        }
+        
+        
+        
+        //after loop ends, if stack is not empty, it means
+        //the heights are in ascending order and i are consecutive!!!
+        //prove: if the heights are not consecutive, 
+        //for example we have stack peek(5,3)bottom, it means heights[4] is < heights[3]
+        //but if so, we have to pop 3 before push 5, then the stack is impossile like peek(5,3)bottom
+        //so the stack must be consecutive
+        
+        while(!stack.isEmpty()){
+            int left = stack.pop();
+            int len = 0;
+            if(stack.isEmpty())
+                len = heights.length - 1 - (-1+1) + 1;
+            else
+                len = heights.length - 1 -stack.peek() - 1 + 1;
+            int area = len * heights[left];
+            max = Math.max(max, area);
+        }
+        
+        return max;
+        
+    }
+}
