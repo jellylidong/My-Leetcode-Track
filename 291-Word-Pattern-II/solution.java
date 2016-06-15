23b561de4d73773dc05f1273d0efae6f9e420194
Dong <jellylidong@users.noreply.github.com>
2016-06-14 23:55:02 -0400

    [291. Word Pattern II][Accepted]committed by LeetCode Extension

--- 291-Word-Pattern-II/solution.java
+++ 291-Word-Pattern-II/solution.java
@@ -1,32 +1,54 @@
 public class Solution {
     HashMap<String, String> map;
+    HashSet<String> valSet;
     public boolean wordPatternMatch(String pattern, String str) {
         map = new HashMap<>();
+        valSet = new HashSet<>();
         map.put("", "");
-        return match(pattern, str);
+        boolean res = match(pattern, str);
+        // for(String key:map.keySet())
+            // System.out.println(key + " " + map.get(key));
+            
+        return res;
     }
     
     public boolean match(String p, String s){
+        
+        if(p.length() == 0 && s.length() != 0)
+            return false;
         if(map.containsKey(p) && map.get(p).equals(s))
             return true;
-        for(int i = 1; i <= p.length(); i++){
-            String ps = p.substring(0, i);
-            if(map.containsKey(ps) && s.startsWith(map.get(ps))){
-                boolean res = match(p.substring(i), s.substring(map.get(ps).length()));
-                if(res)
-                    return true;
-                
-            }
-            else if(!map.containsKey(ps)){
-                for(int j = 1; j <= p.length(); j++){
-                    String ss = s.substring(0, j);
-                    map.put(ps, ss);
-                    boolean res = match(p.substring(i), s.substring(j));
-                    if(res) return true;
-                    map.remove(ps);
-                }
+        
+        String ps = p.substring(0, 1);
+        if(map.containsKey(ps) && s.startsWith(map.get(ps))){
+            boolean res = match(p.substring(1), s.substring(map.get(ps).length()));
+            if(res)
+                return true;
+            
+        }
+        else if(!map.containsKey(ps)){
+            for(int j = 1; j <= s.length(); j++){
+                String ss = s.substring(0, j);
+                if(valSet.contains(ss))
+                    continue;
+                map.put(ps, ss);
+                valSet.add(ss);
+                boolean res = match(p.substring(1), s.substring(j));
+                if(res) return true;
+                map.remove(ps);
+                valSet.remove(ss);
             }
         }
+        
         return false;
     }
+    
+    /*
+    "abab"
+    "redblueredblue"
+    
+    
+    
+    
+    */
 }
