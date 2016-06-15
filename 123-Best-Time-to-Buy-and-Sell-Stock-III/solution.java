e3b2dd612f49db4ce702da2943a14b7c359f9f19
Dong <jellylidong@users.noreply.github.com>
2016-06-15 18:28:37 -0400

    [123. Best Time to Buy and Sell Stock III][Accepted]committed by LeetCode Extension

--- 123-Best-Time-to-Buy-and-Sell-Stock-III/solution.java
+++ 123-Best-Time-to-Buy-and-Sell-Stock-III/solution.java
@@ -1,24 +1,13 @@
 public class Solution {
-    // public int maxProfit(int[] prices) {
-    //     int K = 2;
-    //     int len = prices.length;
-    //     int[][] profit = new int[K+1][len];
-    //     //profit[i][j] means the max profit after jkth transaction and day j
-        
-    //     int max = 0;
-    //     for(int i = 1; i <= K; i++){
-            
-    //         for(int j = 1; j < prices.length; j++){
-                
-    //             for(int m = 0; m < j; m++)
-    //                 preMax = Math.max(preMax, prices[j]-prices[m] + profit[i-1][m]);
-    //             profit[i][j] = Math.max(profit[i][j-1], preMax);
-    //             max = Math.max(max, profit[i][j]);
-    //         }
-    //     }
-        
-    //     return max;
-    // }
+    
+    //profit[i][j] means the max profit with i transactions until day j
+    //note the description, this means it's not necessarily to sell at day j
+    //so when i comes to profit[i][j]
+    //we have two choices
+    //1. we don't make transation at day j, then the profit would be profit[i][j-1]
+    //2. we make a transactiona at day j, then the profit would be prices[j]-prices[x] + profit[i-1][x]
+    //x can be any day bwteen [0, current j)
+    //since prices[j] is instant, so we need to find the max of profit[i-1][x]-prices[x]
     
     public int maxProfit(int[] prices) {
         int K = 2;
@@ -26,13 +15,14 @@ public class Solution {
         if(len == 0)
             return 0;
         int[][] profit = new int[K+1][len];
-        //profit[i][j] means the max profit after jkth transaction and day j
         
         int max = 0;
         for(int k = 1; k <= K; k++){
             int preMax = profit[k-1][0] - prices[0];
             for(int j = 1; j < prices.length; j++){
-
+                //here is a trick, we don't have to go from 0 to j  every time
+                //we only need to update preMax by compare preMax with newly generated profit[i][j]-prices[j]
+                //if we don't do this, TLE
                 profit[k][j] = Math.max(profit[k][j-1], prices[j] + preMax);
                 preMax = Math.max(preMax, profit[k-1][j] - prices[j]);
                 max = Math.max(max, profit[k][j]);
