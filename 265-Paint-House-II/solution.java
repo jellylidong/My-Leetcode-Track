8bfc19204fed93b30a22fab0ebe87d3d6f4ea8f5
Dong <jellylidong@users.noreply.github.com>
2016-06-16 18:58:23 -0400

    [265. Paint House II][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 265-Paint-House-II/solution.java
@@ -0,0 +1,57 @@
+public class Solution {
+    public int minCostII(int[][] costs) {
+        int n = costs.length;
+        if(n == 0)
+            return 0;
+        int k = costs[0].length;
+        if(k == 0)
+            return 0;
+            
+        int[][] dp = new int[n][k];
+        for(int i = 0; i < k; i++)
+            dp[0][i] = costs[0][i];
+        for(int i = 1; i < n; i++){
+            int[] min = findMin(dp[i-1]);
+            for(int j = 0; j < k; k++){
+                dp[i][j] = costs[i][j] + min[j];
+            }
+        }
+        
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < k; i++)
+            min = Math.min(min, dp[n-1][i]);
+            
+        return min;
+    }
+    
+    public int[] findMin(int[] nums){
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        int pre = 0;
+        int id = 0;
+        for(int n: nums)
+            min1 = Math.min(min1, n);
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == min1){
+                pre = nums[i];
+                id = i;
+                nums[i] = Integer.MAX_VALUE;
+                break;
+            }
+        }
+        for(int n: nums)
+            min2 = Math.min(min2, n);
+        nums[id] = pre;
+        
+        int[] ans = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == min1)
+                ans[i] = min2;
+            else if(nums[i] == min2)
+                ans[i] = min1;
+            else
+                nums[i] = min1;
+        }
+        return ans;
+    }
+}
