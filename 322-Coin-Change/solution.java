70bc6ddcf2fa3499293611ab4683b69a4c86549a
Dong <jellylidong@users.noreply.github.com>
2016-06-16 19:35:25 -0400

    [322. Coin Change][Accepted]committed by LeetCode Extension

---
+++ 322-Coin-Change/solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    HashMap<Integer, Integer> map;
+    public int coinChange(int[] coins, int amount) {
+        if(amount <= 0)
+            return 0;
+        
+        Arrays.sort(coins);
+        map = new HashMap<>(); // key is amunt, val is the min ways
+        return helper(coins, amount);
+    }
+    
+    public int helper(int[] coins, int amount){
+        if(amount < 0)
+            return -1;
+        if(amount == 0)
+            return 0;
+        if(map.containsKey(amount))
+            return map.get(amount);
+            
+        int ans = Integer.MAX_VALUE;
+        for(int i = 0; i < coins.length; i++){
+            int cur = helper(coins, amount-coins[i]);
+            if(cur >= 0)
+                ans = Math.min(ans, 1+cur);
+        }
+        ans = ans == Integer.MAX_VALUE? -1: ans;
+        map.put(amount, ans);
+        return ans;
+    }
+}
