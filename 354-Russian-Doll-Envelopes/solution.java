d270897443caad509df96f563e858f3c5efdb5b3
Dong <jellylidong@users.noreply.github.com>
2016-06-17 00:35:48 -0400

    [354. Russian Doll Envelopes][Accepted]committed by LeetCode Extension

--- 354-Russian-Doll-Envelopes/solution.java
+++ 354-Russian-Doll-Envelopes/solution.java
@@ -1,7 +1,25 @@
 public class Solution {
     public int maxEnvelopes(int[][] envelopes) {
-        //longest increasing sequence
+        //similar to longest increasing sequence
         
+        //we need to sort envelopes with ascending w, 
+        //when w equasl, we sort with descending l
+        //thus we can go through all envelopes and find longest increasing l sequence
+        
+        //why do so?
+        //because w are already increasing, 
+        //so we only need increading l,
+        //then we can make sure the next evelope can contains the previous one
+        
+        //why do we use descending l?
+        //eg 
+        //if we use ascending l, the sorted array is [[3,3], [3,4]]
+        //then at the beginning [3,3] is inserted, then we come to [3,4], its inserted after [3,3]
+        //then it becomes [3,3], [3,4], that is to say the length of longest increasing sequence is 2
+        //but actually [3,4] is not able to contains[3,3], so it's wrong
+        //so we use descending l, the sorted array is [3,4], [3,3]
+        //at the beginning [3,4] is inserted, then we come to [3,3], 
+        //it will not be put a positon before (or at) [3,4], the length won't change, it's still 1
         if(envelopes.length == 0)
             return 0;
         int len = 0;
