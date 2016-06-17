3e4f709cbf2eb7de2ca3a8add20b3110cc8a87a5
Dong <jellylidong@users.noreply.github.com>
2016-06-17 18:16:56 -0400

    [76. Minimum Window Substring][Accepted]committed by LeetCode Extension

--- 76-Minimum-Window-Substring/solution.java
+++ 76-Minimum-Window-Substring/solution.java
@@ -20,21 +20,27 @@ public class Solution {
             if(tMap.containsKey(c)){
                 sMap.put(c, sMap.containsKey(c)? sMap.get(c)+1: 1);
                 if(sMap.get(c) <= tMap.get(c)){
-                    count++;
+                    count++;//a valid put, count++
                 }
             }
             if(count == t.length()){
+                //update lo
+                //count == t.length() means all chars in t are covered
+                //but current length can have redundant chars
                 while(!tMap.containsKey(s.charAt(lo)) || sMap.get(s.charAt(lo)) > tMap.get(s.charAt(lo))){
                     if(tMap.containsKey(s.charAt(lo))){
                         sMap.put(s.charAt(lo), sMap.get(s.charAt(lo))-1);
                     }
                     lo++;
                 }
+                //update minLen and minStart
                 if(hi-lo+1 < minLen){
                     minLen = hi-lo+1;
                     minStart = lo; // we must keep the minStart of minLen, it will be used to return result
                 }
                 
+                //after above steps, str start from lo (inclusive) is a valid string covers t
+                //we need to start next loop from lo+1
                 sMap.put(s.charAt(lo), sMap.get(s.charAt(lo))-1);
                 lo++;
                 count--;
