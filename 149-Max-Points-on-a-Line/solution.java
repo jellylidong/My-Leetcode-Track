b82a9db27c40868d6b19b2e8a4fcf41c33e337c7
Dong <jellylidong@users.noreply.github.com>
2016-06-18 18:40:53 -0400

    [149. Max Points on a Line][Accepted]committed by LeetCode Extension

--- 149-Max-Points-on-a-Line/solution.java
+++ 149-Max-Points-on-a-Line/solution.java
@@ -9,15 +9,25 @@
  */
 public class Solution {
     public int maxPoints(Point[] points) {
+        //basic idea
+        //for points[i], find the numbers of points in the same line with points[i] from points[i+1 ... len]
+        //note these numbers does not include points[i]
+        //find the max value from these values
+        //meanwhile, if points[i] == points[j], overlap++ and continue
+        //after find the max value
+        //update answer, ans = Math.max(max+overlap+1, ans), 
+        //the +1 means points[i] itself, the overlap means points same with points[i]
+        
         HashMap<Double, HashMap<Double, Integer>> map = new HashMap<>(); // slope, offset, count
         
         int ans = 0;
         if(points.length <= 2)
             return points.length;
             
-        
-        
         for(int i = 0; i < points.length; i++){
+            //each round, generate the number of points in the same line with points[i]
+            //note these points != points[i]
+            //if points[j] == points[i], overlap++
             map.clear();
             int overlap = 0;
             int max = 0;
@@ -35,6 +45,7 @@ public class Solution {
                 double y1 = (double)points[j].y;
                 
                 if(x0==x1 && y0== y1){
+                    //count the numbers of same points
                     overlap++;
                     continue;
                 }
@@ -68,7 +79,7 @@ public class Solution {
                 max = Math.max(max, map.get(slope).get(offset));
             }
             
-            ans = Math.max(max+overlap+1, ans);
+            ans = Math.max(max+overlap+1, ans); //the real number is points[i] itself(+1) and dup points(+overlap)
         }
         
         return ans;
