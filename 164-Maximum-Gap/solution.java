52ac4203f789fb3c9e7d520ed73189621501da06
Dong <jellylidong@users.noreply.github.com>
2016-06-20 01:28:19 -0400

    [164. Maximum Gap][Accepted]committed by LeetCode Extension

--- 164-Maximum-Gap/solution.java
+++ 164-Maximum-Gap/solution.java
@@ -4,35 +4,43 @@ public class Solution {
             return 0;
         
         int max = Integer.MIN_VALUE;
-        int min = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
         for(int n: nums){
             max = Math.max(max, n);
             min = Math.min(min, n);
         }
         
-        int size = (max-min)/(nums.length-1);
+        int size = (int)Math.ceil((double)(max-min)/(nums.length-1));
+        // 1.2->2, 1.0->1, 1.5->2
+        // System.out.println(size);
         int[] maxs = new int[nums.length-1];
         int[] mins = new int[nums.length-1];
         Arrays.fill(maxs, Integer.MIN_VALUE);
         Arrays.fill(mins, Integer.MAX_VALUE);
         
         for(int i = 0; i < nums.length; i++){
+            if(nums[i] == max || nums[i] == min)
+                continue;
             int id = (nums[i]-min)/size;
-            maxs[id] = Math.max(maxs[i], nums[i]);
-            mins[id] = Math.min(mins[i], nums[i]);
+            maxs[id] = Math.max(maxs[id], nums[i]);
+            mins[id] = Math.min(mins[id], nums[i]);
         }
+        // for(int i = 0; i < nums.length-1; i++){
+        //     // System.out.println(maxs[i] + " " + mins[i]);    
+        // }
         
         int ans = 0;
-        if(maxs[0] != Integer.MIN_VALUE && mins[0] != Integer.MAX_VALUE)
-            ans = maxs[0] - mins[0];
-        for(int i = 1; i < nums.length-1; i++){
-            if(maxs[i] != Integer.MIN_VALUE && mins[i] != Integer.MAX_VALUE)
-                ans = Math.max(ans, maxs[i] - mins[i]);
-            if(mins[i] != Integer.MAX_VALUE && maxs[i-1] != Integer.MIN_VALUE)
-                ans = mins[i] - maxs[i-1];
+        int preMin = min;
+        for(int i = 0; i < nums.length-1; i++){
+            if(mins[i] != Integer.MAX_VALUE){
+                ans = Math.max(mins[i]-preMin, ans);
+                // ans = Math.max(maxs[i]-mins[i], ans);
+                preMin = maxs[i];
+            }
         }
-        
-        return max;
+        // System.out.println(max + " " + preMin);
+        ans = Math.max(ans, max-preMin);
+        return ans;
     }
     
     
