045145532a1cdcd30239425d8ea0f6c8f73ab30e
Dong <jellylidong@users.noreply.github.com>
2016-06-20 15:31:31 -0400

    [224. Basic Calculator][Accepted]committed by LeetCode Extension

--- 224-Basic-Calculator/solution.java
+++ 224-Basic-Calculator/solution.java
@@ -1,37 +1,45 @@
 public class Solution {
     //"(1-(3-4))"
+    //"(7)-(0)+(4)"
     public int calculate(String s) {
-       return helper(0, s);
+       Stack<Integer> stack = new Stack<>();
+       int ans = 0;
+       int sign = 1;
+       int num = 0;
+       
+       for(int i = 0; i < s.length(); i++){
+           char c = s.charAt(i);
+           if(c == '('){
+               stack.push(ans);
+               stack.push(sign);
+               ans = 0;
+               sign = 1;
+               num = 0;
+           }
+           if(c == ')'){
+               ans += sign*num;
+               ans *= stack.pop();
+               ans += stack.pop();
+               num = 0;
+           }
+           if(c == '-'){
+                ans += sign*num;
+                num = 0;
+                sign = -1;
+           }
+           if(c == '+'){
+                ans += sign*num;
+                num = 0;
+                sign = 1;
+           }
+           if(c >= '0' && c <= '9'){
+               num = 10*num + (c - '0');
+           }
+       }
+       
+       ans += sign*num;
+       return ans;
     }
     
-    public int helper(int start, String s){
-        int ans = 0;
-        int sign = 1;
-        for(int i = start; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(c == '(')
-                return ans + sign*helper(i+1, s);
-            if(c == ')'){
-                if(i < s.length()){
-                    if(s.charAt(i+1) == '+')
-                        return ans + helper(i+2, s);
-                    else
-                        return ans - helper(i+2, s);
-                }
-                else    return ans;
-            }
-            if(c == '+')
-                sign = 1;
-            if(c == '-')
-                sign = -1;
-            if(c >= '0' && c <= '9'){
-                int j = i+1;
-                while(j < s.length() && s.charAt(j) >= '0' && s.charAt(j) <= '9')
-                    j++;
-                int num = Integer.parseInt(s.substring(i, j));
-                ans += sign*num;
-            }
-        }
-        return ans;
-    }
+    
 }
