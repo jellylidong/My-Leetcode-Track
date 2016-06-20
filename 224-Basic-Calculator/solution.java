758cc0c03628a3c7537cb8792cc700b14668dd0d
Dong <jellylidong@users.noreply.github.com>
2016-06-20 11:57:59 -0400

    [224. Basic Calculator][Runtime Error]committed by LeetCode Extension

---
+++ 224-Basic-Calculator/solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public int calculate(String s) {
+        Stack<String> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder(s);
+        int sign = 1;
+        for(int i = 0; i < sb.length(); i++){
+            char c = sb.charAt(i);
+            if(c == '(')
+                stack.push("(");
+            else if(c == '+')
+                sign = 1;
+            else if(c == '-')
+                sign = -1;
+            else if(c == ')'){
+                String tmp = stack.pop();
+                stack.pop();
+                stack.push(tmp);
+            }
+            else{
+                int j = i+1;
+                while(j < s.length() && sb.charAt(j) >= '0' && sb.charAt(j) <= '9')
+                    j++;
+                int num = Integer.parseInt(sb.substring(i, j));
+                if(stack.isEmpty() || stack.peek().equals("("))
+                    stack.push(sign == 1? num+"": "-"+num);
+                else{
+                    int num2 = Integer.parseInt(stack.pop());
+                    num = sign*num + num2;
+                    stack.push(""+num);
+                }
+            }
+        }
+        
+        return Integer.parseInt(stack.pop());
+    }
+}
