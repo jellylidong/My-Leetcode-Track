57656973efffc633513b6e1748015d8c4c181cd5
Dong <jellylidong@users.noreply.github.com>
2016-06-22 14:26:25 -0400

    [139. Word Break][Accepted]committed by LeetCode Extension

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -1,31 +1,16 @@
 public class Solution {
-    
-    /*
-    Thought: compared to other sumbissions (which is wrong somehow but the method should be correct, but can't find why)
-    In this method, instead of using dfs to find the deeper result and then compute the shallower one with the deeper one
-    We compute from shallow to deep
-    This is exactly DP
-    So when DFS is complex to implement
-    Consider forward DP (from shallow to deep)
-    */
-    public boolean wordBreak(String s, Set<String> dict) {
-        int len = s.length();
-        boolean[] res = new boolean[len+1];
-        //res[i] is true if s.substring(0, i) is breakable
+    public boolean wordBreak(String s, Set<String> wordDict) {
+        boolean[] res = new boolean[s.length()+1];
         res[0] = true;
-        
         for(int i = 1; i <= s.length(); i++){
             for(int j = 0; j < i; j++){
-                res[i] = res[j] && dict.contains(s.substring(j, i));
+                String cur = s.substring(j, i);
+                res[i] = res[j] && wordDict.contains(cur);
                 if(res[i])
                     break;
             }
         }
         
-        return res[len];
-        
-        
+        return res[s.length()];
     }
-    
-    
 }
