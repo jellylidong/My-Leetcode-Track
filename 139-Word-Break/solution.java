2844eb33da990588b5f09a5414776bc469fa0a55
Dong <jellylidong@users.noreply.github.com>
2016-06-22 16:38:30 -0400

    [139. Word Break][Runtime Error]committed by LeetCode Extension

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -1,16 +1,25 @@
 public class Solution {
     public boolean wordBreak(String s, Set<String> wordDict) {
-        boolean[] res = new boolean[s.length()+1];
-        res[0] = true;
-        for(int i = 1; i <= s.length(); i++){
-            for(int j = 0; j < i; j++){
-                String cur = s.substring(j, i);
-                res[i] = res[j] && wordDict.contains(cur);
-                if(res[i])
-                    break;
+        Queue<Integer> q = new LinkedList<>();
+        HashSet<Integer> visited = new HashSet<>();
+        
+        q.offer(0);
+        
+        while(!q.isEmpty()){
+            int start = q.poll();
+            if(start == s.length())
+                return true;
+                
+            for(String str: wordDict){
+                if(s.substring(start, start+str.length()).equals(str)){
+                    if(!visited.contains(start+str.length())){
+                        q.offer(start+str.length());
+                        visited.add(start+str.length());
+                    }
+                }
             }
         }
         
-        return res[s.length()];
+        return false;
     }
 }
