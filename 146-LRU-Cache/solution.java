f882a60b6f781a5a436117841563349708608c5b
Dong <jellylidong@users.noreply.github.com>
2016-06-23 18:58:47 -0400

    [146. LRU Cache][Runtime Error]committed by LeetCode Extension

--- 146-LRU-Cache/solution.java
+++ 146-LRU-Cache/solution.java
@@ -1,21 +1,21 @@
 public class LRUCache {
-    
+
     node cur;
     node head;
     int capacity;
     int count;
     HashMap<Integer, node> map;
-   
-    
+
+
     public LRUCache(int capacity) {
         this.capacity = capacity;
         this.count = 0;
-        this.cur = new node(0, 0);
+        this.cur = new node(-1, -1);
         head = cur;
         this.map = new HashMap<>();
-        
+
     }
-    
+
     public int get(int key) {
         if(map.containsKey(key)){
             node curNode = map.get(key);
@@ -33,30 +33,41 @@ public class LRUCache {
         else
             return -1;
     }
-    
+
     public void set(int key, int value) {
         int n = get(key);
         if(n == -1){
+            if(count == capacity){
+                node next = head.next;
+                head.next = next.next;
+                if(next.next != null)
+                    next.next.pre = head;
+                else
+                    cur = head;
+                count--;
+                map.remove(next.key);
+            }
             node newNode = new node(key, value);
             cur.next = newNode;
             newNode.pre = cur;
             cur = newNode;
             map.put(key, newNode);
             count++;
-            if(count > capacity){
-                node next = head.next;
-                head.next = next.next;
-                if(head.next != null)
-                    head.next.pre = head;
-                count--;
-                map.remove(next.key);
-            }
         }
         else{
             map.get(key).val = value;
         }
     }
-    
+
+    public void print(){
+        node n = head;
+        while(n != null) {
+            System.out.print(n.val + "+" + n.key + ", ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+
     public class node{
         node pre;
         node next;
