e2f119771f95b472df03c921c73bbc289d3a1554
Dong <jellylidong@users.noreply.github.com>
2016-06-25 15:37:44 -0400

    [22. Generate Parentheses][Accepted]committed by LeetCode Extension

--- 22-Generate-Parentheses/solution.java
+++ 22-Generate-Parentheses/solution.java
@@ -1,29 +1,22 @@
 public class Solution {
-    List<String> ans;
     public List<String> generateParenthesis(int n) {
-        ans = new ArrayList<>();
-        generateParenthesis(n, n, "");
+        List<String>ans = new ArrayList<>();
+        helper(n, n, "", ans);
         return ans;
     }
     
-    //left and right are the number of left and right bracket
-    public void generateParenthesis(int left, int right, String cur){
-        if(left == 0 && right == 0){
+    public void helper(int left, int right, String cur, List<String>ans){
+        if(left == 0 && right == 0)
             ans.add(cur);
-            return;   
-        }
-        if(cur.length() == 0)
-            generateParenthesis(left-1, right, "(");
         else{
-            //if we want to put a "(" next, 
-            //we must guarantee that the left backet number >= 1 and right bracket number >= left-1
-            if(left-1 >= 0 && right >= left-1)
-                generateParenthesis(left-1, right, cur+"(");
-                
-            //if we want to put a ")" next,
-            //we must guarantee that right bracket number >= 1 and right-1 >= left
-            if(right-1 >= 0 && right-1 >= left)
-                generateParenthesis(left, right-1, cur+")");
+            if(cur.length() == 0)
+                helper(left-1, right, "(", ans);
+            else{
+                if(left - 1 >= 0 && right >= left-1)
+                    helper(left-1, right, cur+"(", ans);
+                if(right - 1 >= 0 && right-1 >= left)
+                    helper(left, right-1, cur+")", ans);
+            }
         }
     }
 }
