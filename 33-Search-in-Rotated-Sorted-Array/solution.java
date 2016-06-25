8c11201b561e86777fb9c8a79a3f0452a04ea6c9
Dong <jellylidong@users.noreply.github.com>
2016-06-25 17:54:46 -0400

    [33. Search in Rotated Sorted Array][Runtime Error]committed by LeetCode Extension

--- 33-Search-in-Rotated-Sorted-Array/solution.java
+++ 33-Search-in-Rotated-Sorted-Array/solution.java
@@ -1,11 +1,4 @@
 public class Solution {
-    
-    //!!!!!!!!!!!!
-    /*
-    tips for binary search
-    always keep the nums[mid] == target condition so that avoid dead loop and early termination
-    also, when change bound, always use mid+1 or mid-1 so that avoid dead loop
-    */
     public int search(int[] nums, int target) {
         int lo = 0;
         int hi = nums.length-1;
@@ -14,20 +7,20 @@ public class Solution {
             int mid = lo + (hi-lo)/2;
             if(nums[mid] == target)
                 return mid;
-            if(nums[mid] < nums[hi]){
-                if(nums[mid] < target && target <= nums[hi])
-                    lo = mid+1;
-                else
-                    hi = mid-1;
-            }
-            else{
+            if(nums[mid] > nums[lo]){
                 if(nums[lo] <= target && target < nums[mid])
                     hi = mid-1;
                 else
                     lo = mid+1;
             }
+            else{//nums[mid] < nums[lo]
+                if(nums[mid] < target && target <= nums[hi])
+                    lo = mid+1;
+                else
+                    hi = mid-1;
+            }
         }
         
-        return nums[lo] == target? lo:-1;
+        return nums[hi] == target? lo: -1;
     }
 }
