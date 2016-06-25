b00ff5bd356021eb5326038a01a509317c124354
Dong <jellylidong@users.noreply.github.com>
2016-06-25 14:10:34 -0400

    [10. Regular Expression Matching][Accepted]committed by LeetCode Extension

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -3,25 +3,26 @@ public class Solution {
         int ls = s.length();
         int lp = p.length();
         
-        boolean[][] dp = new boolean[ls+1][lp+1]; //dp[i][j] s.substring(0, i) macthes p.substring(0, j)
+        boolean[][] dp = new boolean[ls+1][lp+1];
+        //dp[i][j] means s.substring(0, i) matches p.substirng(0, j);
         dp[0][0] = true;
         
         for(int i = 1; i <= lp; i++)
-            dp[0][i] = i > 1 && dp[0][i-2] && p.charAt(i-1) == '*';
+            dp[0][i] = i > 1 && p.charAt(i-1) == '*' && dp[0][i-2];
         for(int i = 1; i <= ls; i++)
             dp[i][0] = false;
             
         for(int i = 1; i <= ls; i++){
             for(int j = 1; j <= lp; j++){
-                if(p.charAt(j-1) != '*')
-                    dp[i][j] = dp[i-1][j-1] && (p.charAt(j-1) == '.' || p.charAt(j-1) == s.charAt(i-1));
-                else{
-                    dp[i][j] = dp[i][j-2] ||
+                if(p.charAt(j-1) == '*'){
+                    dp[i][j] = dp[i][j-2] || 
                                (dp[i-1][j] && (p.charAt(j-1-1) == '.' || p.charAt(j-1-1) == s.charAt(i-1)));
+                }
+                else{
+                    dp[i][j] = dp[i-1][j-1] && (p.charAt(j-1) == '.' || p.charAt(j-1) == s.charAt(i-1));
                 }
             }
         }
-        
         return dp[ls][lp];
     }
 }
