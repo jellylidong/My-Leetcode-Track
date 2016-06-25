99e73b036f3c4345c13cf862e7a856cb66369668
Dong <jellylidong@users.noreply.github.com>
2016-06-25 18:59:43 -0400

    [81. Search in Rotated Sorted Array II][Accepted]committed by LeetCode Extension

--- 81-Search-in-Rotated-Sorted-Array-II/solution.java
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.java
@@ -5,26 +5,26 @@ public class Solution {
         
         while(lo < hi){
             int mid = lo + (hi-lo)/2;
+            
             if(nums[mid] == target)
                 return true;
                 
-            //pass by all the dup at hi
-            if(nums[mid] < nums[hi]){
-                if(nums[mid] < target && target <= nums[hi])
-                    lo = mid + 1;
-                else
-                    hi = mid;
-            }
-            else if(nums[mid] == nums[hi]){
-                hi--;
-            }
-            else{//nums[mid] > nums[hi]
+            if(nums[lo] < nums[mid]){
                 if(nums[lo] <= target && target < nums[mid])
-                    hi = mid;
+                    hi = mid-1;
                 else
                     lo = mid+1;
             }
-            
+            else if(nums[lo] == nums[mid])
+                lo++;
+            else if(nums[mid] == nums[hi])
+                hi--;
+            else{
+                if(nums[mid] < target && target <= nums[hi])
+                    lo = mid+1;
+                else
+                    hi = mid-1;
+            }
         }
         
         return nums[lo] == target;
