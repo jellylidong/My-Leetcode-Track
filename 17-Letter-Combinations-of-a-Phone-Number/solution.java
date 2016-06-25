af13e037c5852901e8c34a288ee75c3395548dee
Dong <jellylidong@users.noreply.github.com>
2016-06-25 14:53:16 -0400

    [17. Letter Combinations of a Phone Number][Runtime Error]committed by LeetCode Extension

--- 17-Letter-Combinations-of-a-Phone-Number/solution.java
+++ 17-Letter-Combinations-of-a-Phone-Number/solution.java
@@ -1,24 +1,20 @@
 public class Solution {
     public List<String> letterCombinations(String digits) {
-        String[] key = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        List<String> ans = new ArrayList<>();
         if(digits.length() == 0)
-            return ans;
+            return new ArrayList<String>();
+        String[] ss = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> ans = new ArrayList<>();
         ans.add("");
-        
         for(int i = 0; i < digits.length(); i++){
-            int cur = digits.charAt(i) - '0';
-            String curStr = key[cur];
+            String cur = ss[digits.charAt(i) - '0'];
             List<String> newAns = new ArrayList<>();
-            for(int j = 0; j < curStr.length(); j++){
+            for(int j = 0; j < cur.length(); j++){
                 for(String s: ans){
-                    newAns.add(s + curStr.charAt(j));
+                    ans.add(s + cur.charAt(j));
                 }
             }
             ans = newAns;
         }
-        
         return ans;
-        
     }
 }
