8a7f7e0f9956df7157caa52273fe9d9cc5eeac20
Dong <jellylidong@users.noreply.github.com>
2016-06-25 14:44:58 -0400

    [13. Roman to Integer][Accepted]committed by LeetCode Extension

--- 13-Roman-to-Integer/solution.java
+++ 13-Roman-to-Integer/solution.java
@@ -1,76 +1,28 @@
 public class Solution {
     public int romanToInt(String s) {
-        int sum = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
         
-        for(int i = 0; i < s.length();){
-            char c = s.charAt(i);
-            if(c == 'I'){
-                if(i+1 < s.length() && s.charAt(i+1) == 'V'){
-                    sum += 4;
-                    i += 2;
-                }
-                else if(i+1 < s.length() && s.charAt(i+1) == 'X'){
-                    sum += 9;
-                    i += 2;
-                }
-                else{
-                    sum += 1;
-                    i++;
-                }
-            }
-            
-            else if(c == 'V'){
-                sum += 5;
-                i++;
-            }
-            
-            else if(c == 'X'){
-                 if(i+1 < s.length() && s.charAt(i+1) == 'L'){
-                    sum += 40;
-                    i += 2;
-                }
-                else if(i+1 < s.length() && s.charAt(i+1) == 'C'){
-                    sum += 90;
-                    i += 2;
-                }
-                else{
-                    sum += 10;
-                    i++;
-                }
-            }
-            
-            else if(c == 'L'){
-                sum += 50;
-                i++;
-            }
-            
-            else if(c == 'C'){
-                 if(i+1 < s.length() && s.charAt(i+1) == 'D'){
-                    sum += 400;
-                    i += 2;
-                }
-                else if(i+1 < s.length() && s.charAt(i+1) == 'M'){
-                    sum += 900;
-                    i += 2;
-                }
-                else{
-                    sum += 100;
-                    i++;
-                }
-            }
-            
-            else if(c == 'D'){
-                sum += 500;
-                i++;
-            }
-            else if(c == 'M'){
-                sum += 1000;
-                i++;
-            }
-            
-            
+        int pre = map.get(s.charAt(s.length()-1));
+        int ans = pre;
+        
+        for(int i = s.length()-2; i >= 0; i--){
+            int cur = map.get(s.charAt(i));
+            //if cur >= its right number, plus cur
+            //else minus cur
+            if(cur >= pre)
+                ans += cur;
+            else
+                ans -= cur;
+            pre = cur;
         }
         
-        return sum;
+        return ans;
     }
 }
