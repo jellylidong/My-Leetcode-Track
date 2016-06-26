8a7f53914fc4e8fdbdd315266cf8e740e16ac0c0
Dong <jellylidong@users.noreply.github.com>
2016-06-26 18:04:54 -0400

    [133. Clone Graph][Runtime Error]committed by LeetCode Extension

--- 133-Clone-Graph/solution.java
+++ 133-Clone-Graph/solution.java
@@ -7,57 +7,20 @@
  * };
  */
 public class Solution {
-    
-    /*
-    {0,0,0}
-    {0,1,5#1,2,5#2,3#3,4,4#4,5,5#5}
-    */
-    
-    // HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
-    // HashSet<UndirectedGraphNode> visited = new HashSet<>();
-    // public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-    //     if(node == null)
-    //         return null;
-    //     UndirectedGraphNode cur;
-    //     if(!map.containsKey(node)){
-    //          cur = new UndirectedGraphNode(node.label);
-    //          map.put(node, cur);
-    //     }
-    //     else
-    //         cur = map.get(node);
-            
-    //     visited.add(node);
-    //     for(UndirectedGraphNode n: node.neighbors){
-            
-    //             if(map.containsKey(n))
-    //                 cur.neighbors.add(map.get(n));
-    //             else{
-    //                 UndirectedGraphNode newNode = new UndirectedGraphNode(n.label);
-    //                 map.put(n, newNode);
-    //                 cur.neighbors.add(newNode);
-    //             }
-    //             if(!visited.contains(n))
-    //                 cloneGraph(n);
-            
-    //     }
-        
-    //     return cur;
-    // }
-    
-    //above code works well, just another more method with similar thought but more concise code
     HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
         if(node == null)
             return null;
+            
         if(map.containsKey(node))
             return map.get(node);
-            
-        UndirectedGraphNode cur = new UndirectedGraphNode(node.label);
-        map.put(node, cur);
-        for(UndirectedGraphNode n: node.neighbors){
-            cur.neighbors.add(cloneGraph(n));
+        else{
+            UndirectedGraphNode cur = new UndirectedGraphNode(node.label);
+            for(UndirectedGraphNode n: node.neighbors){
+                cur.neighbors.add(cloneGraph(n));
+            }
+            map.put(node, cur);
+            return cur;
         }
-        
-        return cur;
     }
 }
