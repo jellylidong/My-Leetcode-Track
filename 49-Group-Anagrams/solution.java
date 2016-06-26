21ede174ddf11c0070128bf24e578e85a5f3e429
Dong <jellylidong@users.noreply.github.com>
2016-06-26 00:52:14 -0400

    [49. Group Anagrams][Accepted]committed by LeetCode Extension

--- 49-Group-Anagrams/solution.java
+++ 49-Group-Anagrams/solution.java
@@ -1,30 +1,15 @@
 public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Arrays.sort(strs);
-        
         HashMap<String, List<String>> map = new HashMap<>();
-        List<String> keys = new ArrayList<>();
         for(String s: strs){
             char[] cs = s.toCharArray();
             Arrays.sort(cs);
-            String key = new String(cs);
-            if(map.containsKey(key))
-                map.get(key).add(s);
-            else{
-                List<String> list = new ArrayList<>();
-                list.add(s);
-                keys.add(key);
-                map.put(key, list);
-            }
+            String newStr = new String(cs);
+            if(!map.containsKey(newStr))
+                map.put(newStr, new ArrayList<String>());
+            map.get(newStr).add(s);
         }
         
-        List<List<String>> ans = new ArrayList<>();
-        for(String key: keys){
-            ans.add(map.get(key));
-        }
-        
-        return ans;
-        
-        
+        return new ArrayList<List<String>>(map.values());
     }
 }
