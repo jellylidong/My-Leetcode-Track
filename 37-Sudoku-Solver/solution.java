625de8a163c552cdfe1c1cc2c43d18261c4ea670
Dong <jellylidong@users.noreply.github.com>
2016-06-25 22:49:25 -0400

    [37. Sudoku Solver][Accepted]committed by LeethujiaoCode Extension

--- 37-Sudoku-Solver/solution.java
+++ 37-Sudoku-Solver/solution.java
@@ -1,82 +1,55 @@
 public class Solution {
-    HashSet[] col;
-    HashSet[] row;
-    HashSet[] sqr;
     public void solveSudoku(char[][] board) {
-        int len = board.length;
-        col = new HashSet[len];
-        row = new HashSet[len];
-        sqr = new HashSet[len];
-        for(int i = 0; i < len; i++){
+        HashSet[] col = new HashSet[9];
+        HashSet[] row = new HashSet[9];
+        HashSet[] sqr = new HashSet[9];
+        for(int i = 0; i < 9; i++){
             col[i] = new HashSet<Character>();
             row[i] = new HashSet<Character>();
             sqr[i] = new HashSet<Character>();
         }
         
-        
-        //if we want to use hashset to store already used numbers, 
-        //we must first store the already used number of intial board first
-        //becuase in later traverse, assume we meet a place need to be filled,
-        //if we don't store the already used values, it's likely that we use a number that is already used in current later positions
-        for(int i = 0; i < len; i++){
-            for(int j = 0; j < len; j++){
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
                 char c = board[i][j];
-                if(c != '.' ){
-                    row[i].add(c);
+                if(c != '.'){
                     col[j].add(c);
-                    
-                    //the trick for transform i,j to 9-number sqr set
-                    //note that row 0,1,2 can only be in sqr[0,1,1], row 3,4,5 can only be in sqr[3,4,5]
-                    //so we can first decide their first part by i/3*3
-                    //then for row 1,2,3, which sqr they should be in depends on j, the second part j/3
-                    //so for board[i][j], it shuold be at sqr[i/3*3 + j/3]
-                    sqr[i/3*3+j/3].add(c);
+                    row[i].add(c);
+                    sqr[i/3*3 + j/3].add(c);
                 }
             }
         }
         
-        solve(board, 0, 0);
+        helper(board, 0, col, row, sqr);
     }
     
-    public boolean solve(char[][] board, int r, int c){
-        if(r == board.length)
+    public boolean helper(char[][] board, int n, HashSet[] col, HashSet[] row, HashSet[] sqr){
+        if(n == 81)
             return true;
-        
-        char ch = board[r][c];
-        if(ch != '.'){
-            // row[r].add(ch);
-            // col[c].add(ch);
-            // sqr[r/3*3+c/3].add(ch);
-            if(c == board[0].length-1)
-                return solve(board, r+1, 0);
-            else
-                return solve(board, r, c+1);
-        }
         else{
-            for(char cc = '1'; cc <= '9'; cc++){
-                if(!row[r].contains(cc) && !col[c].contains(cc) && !sqr[r/3*3+c/3].contains(cc)){
-                    row[r].add(cc);
-                    col[c].add(cc);
-                    sqr[r/3*3+c/3].add(cc);
-                    board[r][c] = cc;
-                    boolean res = false;
-                    if(c == board[0].length-1)
-                        res = solve(board, r+1, 0);
-                    else
-                        res = solve(board, r, c+1);
-                    if(res == true)
-                        return true;
-                    else{
-                        row[r].remove(cc);
-                        col[c].remove(cc);
-                        sqr[r/3*3+c/3].remove(cc);
-                        board[r][c] = '.';
-                        res = false;
+            int i = n/9;
+            int j = n%9;
+            if(board[i][j] != '.')
+                return helper(board, n+1, col, row, sqr);
+            else{
+                for(char c = '1'; c <= '9'; c++){
+                    if(!col[j].contains(c) && !row[i].contains(c) && !sqr[i/3*3 + j/3].contains(c)){
+                        board[i][j] = c;
+                        col[j].add(c);
+                        row[i].add(c);
+                        sqr[i/3*3+j/3].add(c);
+                        if(helper(board, n+1, col, row, sqr))
+                            return true;
+                        else{
+                            board[i][j] = '.';
+                            col[j].remove(c);
+                            row[i].remove(c);
+                            sqr[i/3*3+j/3].remove(c);
+                        }
                     }
                 }
             }
         }
-        
         return false;
     }
 }
