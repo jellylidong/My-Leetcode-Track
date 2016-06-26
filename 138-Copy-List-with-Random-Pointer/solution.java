750120392f0b84fc9fdc5c155327d5c742e61892
Dong <jellylidong@users.noreply.github.com>
2016-06-26 18:15:18 -0400

    [138. Copy List with Random Pointer][Runtime Error]committed by LeetCode Extension

--- 138-Copy-List-with-Random-Pointer/solution.java
+++ 138-Copy-List-with-Random-Pointer/solution.java
@@ -8,46 +8,30 @@
  */
 public class Solution {
     public RandomListNode copyRandomList(RandomListNode head) {
-        //step 1: insert a new node after each node n, which has the same value of n
-        //step 2: update the random pointer of the inserted node
-        //obviously, for the orginal node n, if its random pointer points at node x
-        //then for the inserted node after node n, let's call it i, its random pointer should pointe at x.next
-        
-        //step 3: after above is done, we can get the copied list by extract the very two nodes
         RandomListNode cur = head;
-        while(cur != null){
-            RandomListNode next = cur.next;
-            cur.next = new RandomListNode(cur.label);
-            cur = cur.next;
-            cur.next = next;
-            cur = cur.next;
-        }
         
-        cur = head;
         while(cur != null){
-            RandomListNode next = cur.next;
-            if(cur.random != null)
-                next.random = cur.random.next;
-                
+            RandomListNode node = new RandomListNode(cur.label);
+            node.next = cur.next;
+            cur.next = node;
             cur = cur.next.next;
         }
         
-        RandomListNode helper = new RandomListNode(0);
-        RandomListNode pre = helper;
         cur = head;
         while(cur != null){
-            RandomListNode next = cur.next.next;
-            pre.next = cur.next;
-            pre = pre.next;
-            
-            //recover the original list
-            cur.next = next;
+            RandomListNode node = cur.next;
+            node.random = cur.random.next;
+        }
+        
+        RandomListNode helper = new RandomListNode(0);
+        cur = helper;
+        
+        while(head != null){
+            cur.next = head.next;
+            head = head.next.next;
             cur = cur.next;
         }
         
         return helper.next;
-        
     }
-    
-    //{-1,#}
 }
