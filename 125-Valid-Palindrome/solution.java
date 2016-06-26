53619d3b4095a6e5b78621042bbcd05fbde2234d
Dong <jellylidong@users.noreply.github.com>
2016-06-26 17:32:01 -0400

    [125. Valid Palindrome][Accepted]committed by LeetCode Extension

--- 125-Valid-Palindrome/solution.java
+++ 125-Valid-Palindrome/solution.java
@@ -1,24 +1,17 @@
 public class Solution {
     public boolean isPalindrome(String s) {
+        
+        s = s.toLowerCase();
+        
         int lo = 0;
         int hi = s.length()-1;
         
         while(lo < hi){
-            char clo = s.charAt(lo);
-            char chi = s.charAt(hi);
-            if(!isAN(clo)){
+            while(lo < hi && !isAlphanumeric(s.charAt(lo)))
                 lo++;
-                continue;
-            }
-            if(!isAN(chi)){
+            while(hi > lo && !isAlphanumeric(s.charAt(hi)))
                 hi--;
-                continue;
-            }
-            if(clo >= 'A' && clo <= 'Z')
-                clo += 32;
-            if(chi >= 'A' && chi <= 'Z')
-                chi += 32;
-            if(clo != chi)
+            if(s.charAt(lo) != s.charAt(hi))
                 return false;
             lo++;
             hi--;
@@ -27,7 +20,9 @@ public class Solution {
         return true;
     }
     
-    public boolean isAN(char c){
-        return (c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+    public boolean isAlphanumeric(char c){
+        return (c >= '0' && c <= '9') || 
+               (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z');
     }
 }
