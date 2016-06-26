561ab870d203f0a6d000ac052bc72d034dd13186
Dong <jellylidong@users.noreply.github.com>
2016-06-26 13:01:10 -0400

    [76. Minimum Window Substring][Accepted]committed by LeetCode Extension

--- 76-Minimum-Window-Substring/solution.java
+++ 76-Minimum-Window-Substring/solution.java
@@ -15,36 +15,50 @@ public class Solution {
         
         int lo = 0;
         int hi = 0;
-        int count = 0;
+        int count = 0; // count means how many chars in t current s.substirng covers
         int minLo = 0;
-        int minHi = 0;
+        // int minHi = 0;
         int minLen = Integer.MAX_VALUE;
         
         for(; hi < s.length(); hi++){
             char c = s.charAt(hi);
             if(sMap.containsKey(c)){
                 sMap.put(c, sMap.get(c)+1);
-                count++;
-            
-                while(sMap.get(c) > tMap.get(c)){
-                    char c2 = s.charAt(lo++);
-                    if(sMap.containsKey(c2)){
-                        sMap.put(c2, sMap.get(c2)-1);
-                        count--;
-                    }
-                }
-                while(!sMap.containsKey(s.charAt(lo)))
-                    lo++;
+                
+                //after we add 1, sMap.get(c) still <= tMap.get(c)
+                //it means the add is valid
+                if(sMap.get(c) <= tMap.get(c))
+                    count++;
             }
+            
+            //all chars in t are covered now
             if(count == t.length()){
+                
+                //clear redundant begginning chars
+                //1. chars that are not in t
+                //2. chars are in t but there are too many in current s.substring
+                while(!sMap.containsKey(s.charAt(lo)) || sMap.get(s.charAt(lo)) > tMap.get(s.charAt(lo))){
+                    if(sMap.containsKey(s.charAt(lo)))
+                        sMap.put(s.charAt(lo), sMap.get(s.charAt(lo))-1);
+                    lo++;
+                }
+                
                 if(hi-lo+1 < minLen){
                     minLo = lo;
-                    minHi = hi;
+                    // minHi = hi;
                     minLen = hi-lo+1;
                 }
+                
+                //after update the minLen and minLo, we need to start from next position, ie lo+1
+                sMap.put(s.charAt(lo), sMap.get(s.charAt(lo))-1);
+                count--;
+                lo++;
             }
         }
         
-        return s.substring(minLo, minLo+minLen);
+        return minLen == Integer.MAX_VALUE? "" :s.substring(minLo, minLo+minLen);
     }
+    
+    //"caae" "cae"
+
 }
