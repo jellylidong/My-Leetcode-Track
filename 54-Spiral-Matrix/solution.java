849bcad4c6110f870228c43256b9263d6e52b889
Dong <jellylidong@users.noreply.github.com>
2016-06-26 01:43:30 -0400

    [54. Spiral Matrix][Runtime Error]committed by LeetCode Extension

--- 54-Spiral-Matrix/solution.java
+++ 54-Spiral-Matrix/solution.java
@@ -1,50 +1,32 @@
 public class Solution {
     public List<Integer> spiralOrder(int[][] m) {
-        List<Integer> ans = new ArrayList<>();
-
-        int h = m.length;
-        if(h == 0)
-            return ans;
-        int n = m[0].length;
-        
+        if(m.length == 0)
+            return new ArrayList<Integer>();
+        if(m[0].length == 0)
+            return new ArrayList<Integer>();
+            
         int left = 0;
-        int right = n-1;
         int up = 0;
-        int down = h-1;
+        int right = m[0].length-1;
+        int down = m.length-1;
         
-        int count = 0;
-        while(count < h*n){
-            //go right
-            for(int i = left; count < h*n && i <= right; i++){
+        List<Integer> ans = new ArrayList<>();
+        while(left <= right && up <= down){
+            for(int i = left; i <= right; i++)
                 ans.add(m[up][i]);
-                count++;
-            }
             up++;
-            // if(count >= h*n)    break;
             
-            //go down
-            for(int i = up; count < h*n && i <= down; i++){
+            for(int i = up; i <= down; i++)
                 ans.add(m[i][right]);
-                count++;
-            }
             right--;
-            // if(count >= h*n)    break;
             
-            //go left
-            for(int i = right; count < h*n && i >= left; i--){
+            for(int i = right; i >= left; i--)
                 ans.add(m[down][i]);
-                count++;
-            }
             down--;
-            // if(count >= h*n)    break;
             
-            //go up
-            for(int i = down; count < h*n && i >= up; i--){
+            for(int i = down; i >= up; i++)
                 ans.add(m[i][left]);
-                count++;
-            }
             left++;
-            // if(count >= h*n)    break;
         }
         
         return ans;
