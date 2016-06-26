df7d1a6ef625fe3e3d07ca64d738de2286d01f10
Dong <jellylidong@users.noreply.github.com>
2016-06-26 12:26:55 -0400

    [76. Minimum Window Substring][Runtime Error]committed by LeetCode Extension

--- 76-Minimum-Window-Substring/solution.java
+++ 76-Minimum-Window-Substring/solution.java
@@ -1,55 +1,50 @@
 public class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character, Integer> tMap = new HashMap<>();
         HashMap<Character, Integer> sMap = new HashMap<>();
+        HashMap<Character, Integer> tMap = new HashMap<>();
         
         for(int i = 0; i < t.length(); i++){
             char c = t.charAt(i);
             if(tMap.containsKey(c))
                 tMap.put(c, tMap.get(c)+1);
-            else
+            else{
                 tMap.put(c, 1);
+                sMap.put(c, 0);
+            }
         }
         
-        int minLen = Integer.MAX_VALUE;
-        int count = 0;
         int lo = 0;
-        int minStart = 0;
-        for(int hi = 0; hi < s.length(); hi++){
+        int hi = 0;
+        int count = 0;
+        int minLo = 0;
+        int minHi = 0;
+        int minLen = Integer.MAX_VALUE;
+        
+        for(; hi < s.length(); hi++){
             char c = s.charAt(hi);
-            if(tMap.containsKey(c)){
-                sMap.put(c, sMap.containsKey(c)? sMap.get(c)+1: 1);
-                if(sMap.get(c) <= tMap.get(c)){
-                    count++;//a valid put, count++
+            if(sMap.containsKey(c)){
+                sMap.put(c, sMap.get(c)+1);
+                count++;
+            
+                while(sMap.get(c) > tMap.get(c)){
+                    char c2 = s.charAt(lo++);
+                    if(sMap.containsKey(c2)){
+                        sMap.put(c2, sMap.get(c2)-1);
+                        count--;
+                    }
                 }
+                while(!sMap.containsKey(s.charAt(lo)))
+                    lo++;
             }
             if(count == t.length()){
-                //update lo
-                //count == t.length() means all chars in t are covered
-                //but current length can have redundant chars
-                while(!tMap.containsKey(s.charAt(lo)) || sMap.get(s.charAt(lo)) > tMap.get(s.charAt(lo))){
-                    if(tMap.containsKey(s.charAt(lo))){
-                        sMap.put(s.charAt(lo), sMap.get(s.charAt(lo))-1);
-                    }
-                    lo++;
-                }
-                //update minLen and minStart
                 if(hi-lo+1 < minLen){
+                    minLo = lo;
+                    minHi = hi;
                     minLen = hi-lo+1;
-                    minStart = lo; // we must keep the minStart of minLen, it will be used to return result
                 }
-                
-                //after above steps, str start from lo (inclusive) is a valid string covers t
-                //we need to start next loop from lo+1
-                sMap.put(s.charAt(lo), sMap.get(s.charAt(lo))-1);
-                lo++;
-                count--;
             }
-            
         }
         
-        if(minLen == Integer.MAX_VALUE)
-            return "";
-        return s.substring(minStart, minStart+minLen);
+        return s.substring(minLo, minLo+minLen);
     }
 }
