312d59f90cdafb0e4958e9707942e5040638ca45
Dong <jellylidong@users.noreply.github.com>
2016-06-27 11:15:00 -0400

    [291. Word Pattern II][Accepted]committed by LeetCode Extension

--- 291-Word-Pattern-II/solution.java
+++ 291-Word-Pattern-II/solution.java
@@ -1,27 +1,35 @@
 public class Solution {
     public boolean wordPatternMatch(String pattern, String str) {
         HashMap<Character, String> map = new HashMap<>();
+        
+        //different pattern should not have the same value
         HashSet<String> valSet = new HashSet<>();
         return helper(pattern, str, map, valSet);
     }
     
     public boolean helper(String pattern, String str, HashMap<Character, String> map, HashSet<String> valSet){
+        
+        //all pattern and str are used up, it means all matches, so return true;
         if(pattern.length() == 0 && str.length() == 0)
             return true;
             
+        //one char of pattern should matches at least one char in str
         if(pattern.length() > str.length())
             return false;
             
+        //when pattern is used up but str is not, return false
         if(pattern.length() == 0 && str.length() != 0)
             return false;
             
         char c = pattern.charAt(0);
+        //if we already have find c's value, and str starts with this value, we can continue with pattern's next char
         if(map.containsKey(c) && str.startsWith(map.get(c)))
             return helper(pattern.substring(1), str.substring(map.get(c).length()), map, valSet);
+        //else if we don't find c's value try from str's beginning
         else if(!map.containsKey(c)){
             for(int i = 1; i <= str.length(); i++){
                 String sub = str.substring(0, i);
-                if(valSet.contains(sub))
+                if(valSet.contains(sub)) // this value is already used, we should not use use value
                     continue;
                 else{
                     map.put(c, sub);
@@ -30,12 +38,14 @@ public class Solution {
                     if(res == true)
                         return true;
                     else{
+                        //revocer the map and valSet if current value is invalid
                         map.remove(c);
                         valSet.remove(sub);
                     }
                 }
             }
         }
+        //if we don't find c's value and we can not find c's value with current pattern and str, return false
         return false;    
     }
 }
