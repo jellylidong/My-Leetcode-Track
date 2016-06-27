8f5e250d68424612dc51f75edf506e0bd03a02a7
Dong <jellylidong@users.noreply.github.com>
2016-06-27 18:55:32 -0400

    [230. Kth Smallest Element in a BST][Accepted]committed by LeetCode Extension

--- 230-Kth-Smallest-Element-in-a-BST/solution.java
+++ 230-Kth-Smallest-Element-in-a-BST/solution.java
@@ -24,4 +24,23 @@ public class Solution {
             
         return 1 + count(root.left) + count(root.right);
     }
+    
+    //if the BST is modifiable, we nned to build a tree with count in each node, ie
+    /*
+    public class nodeWithCount{
+        nodeWithCount left;
+        nodeWithCount right;
+        int val;
+        int count = 0;
+        node(int val){
+            this.val = val;
+        }
+    }
+    
+    //the count is the amount of children node of current node
+    //once we have this count value, we don't have to count the children nodes, we can use it directly
+    //so build such a tree take O(n)
+    //insert and delet take O(n)
+    //find the kth smallest number take O(height) ie O(lgn)
+    */
 }
