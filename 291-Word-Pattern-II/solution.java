1db1297956cbe4a412695d1d0ea7b78a6e80bd28
Dong <jellylidong@users.noreply.github.com>
2016-06-27 11:09:04 -0400

    [291. Word Pattern II][Accepted]committed by LeetCode Extension

--- 291-Word-Pattern-II/solution.java
+++ 291-Word-Pattern-II/solution.java
@@ -1,62 +1,41 @@
 public class Solution {
-    HashMap<String, String> map;
-    HashSet<String> valSet;
     public boolean wordPatternMatch(String pattern, String str) {
-        map = new HashMap<>();
-        valSet = new HashSet<>(); 
-        //used to store values
-        //different chars must be map to different strs, ie if a->s1, then b->s1 is invalid
-        
-        // map.put("", "");
-        char[] cs = pattern.toCharArray();
-        return match(cs, 0, str);
-        // for(String key:map.keySet())
-            // System.out.println(key + " " + map.get(key));
-            
-        // return res;
+        HashMap<Character, String> map = new HashMap<>();
+        HashSet<String> valSet = new HashSet<>();
+        return helper(pattern, str, map, valSet);
     }
     
-    public boolean match(char[] cs, int i, String s){
-        
-        // if(cs.length == 0 && s.length() != 0)
-        //     return false;
+    public boolean helper(String pattern, String str, HashMap<Character, String> map, HashSet<String> valSet){
+        if(pattern.length() == 0 && str.length() == 0)
+            return true;
             
-        if(i == cs.length)
-            return s.length() == 0;
-        
-        if(cs.length-i > s.length())
+        if(pattern.length() > str.length())
             return false;
-        
-        // if(map.containsKey(p) && map.get(p).equals(s))
-        //     return true;
-        
-        String ps = "" + cs[i];
-        if(map.containsKey(ps) && s.startsWith(map.get(ps))){
-            return match(cs, i+1, s.substring(map.get(ps).length()));
-        }
-        else if(!map.containsKey(ps)){
-            for(int j = 1; j <= s.length(); j++){
-                String ss = s.substring(0, j);
-                if(valSet.contains(ss))
+            
+        if(pattern.length() == 0 && str.length() != 0)
+            return false;
+            
+        char c = pattern.charAt(0);
+        if(map.containsKey(c) && str.startsWith(map.get(c)))
+            return helper(pattern.substring(1), str.substring(map.get(c).length()), map, valSet);
+        else if(!map.containsKey(c)){
+            for(int i = 1; i <= str.length(); i++){
+                String sub = str.substring(0, i);
+                if(valSet.contains(sub))
                     continue;
-                map.put(ps, ss);
-                valSet.add(ss);
-                boolean res = match(cs, i+1, s.substring(j));
-                if(res) return true;
-                map.remove(ps);
-                valSet.remove(ss);
+                else{
+                    map.put(c, sub);
+                    valSet.add(sub);
+                    boolean res = helper(pattern.substring(1), str.substring(i), map, valSet);
+                    if(res == true)
+                        return true;
+                    else{
+                        map.remove(c);
+                        valSet.remove(sub);
+                    }
+                }
             }
         }
-        
-        return false;
+        return false;    
     }
-    
-    /*
-    "abab"
-    "redblueredblue"
-    
-    
-    
-    
-    */
 }
