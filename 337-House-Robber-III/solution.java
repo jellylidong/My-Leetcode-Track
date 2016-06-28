43ad1102d01a80f2ded1ddcfe8e19365efc74480
Dong <jellylidong@users.noreply.github.com>
2016-06-28 00:20:32 -0400

    [337. House Robber III][Accepted]committed by LeetCode Extension

--- 337-House-Robber-III/solution.java
+++ 337-House-Robber-III/solution.java
@@ -8,19 +8,29 @@
  * }
  */
 public class Solution {
+    HashMap<TreeNode, Integer> map;
     public int rob(TreeNode root) {
+        map = new HashMap<>();
         return Math.max(rob(root, true), rob(root, false));
     }
     
     public int rob(TreeNode root, boolean robbedCur){
         if(root == null)
             return 0;
-            
+        
+        
+         
         if(robbedCur){
-            return root.val + rob(root.left, false) + rob(root.right, false);
+            if(map.containsKey(root))
+                return map.get(root);
+            int val = root.val + rob(root.left, false) + rob(root.right, false);
+            map.put(root, val);
+            return val;
         }
         else{
-            return Math.max(rob(root.left, true), rob(root.left, false)) + Math.max(rob(root.right, true), rob(root.right, false));
+            int val = Math.max(rob(root.left, true), rob(root.left, false)) + Math.max(rob(root.right, true), rob(root.right, false));
+            // map.put(root, val);
+            return val;
         }
     }
 }
