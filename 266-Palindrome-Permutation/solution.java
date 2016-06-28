b102914e9bb9c244807c735242edc70113f71577
Dong <jellylidong@users.noreply.github.com>
2016-06-27 23:29:33 -0400

    [266. Palindrome Permutation][Runtime Error]committed by LeetCode Extension

--- 266-Palindrome-Permutation/solution.java
+++ 266-Palindrome-Permutation/solution.java
@@ -1,20 +1,15 @@
 public class Solution {
     public boolean canPermutePalindrome(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(int i=0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(map.containsKey(c))
-                map.put(c, map.get(c) + 1);
-            else
-                map.put(c, 1);
+        int[] count = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            count[s.charAt(i)-'a']++;
+        }
+        int oddCount = 0;
+        for(int n: count){
+            if(n%2 != 0)
+                oddCount++;
         }
         
-        int odd = 0;
-        for(Character c: map.keySet()){
-            if(map.get(c)%2 == 1)
-                odd++;
-        }
-        
-        return odd <= 1;
+        return oddCount <= 1;
     }
 }
