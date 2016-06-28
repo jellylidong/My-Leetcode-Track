2925915942ac5398d2055bef7c6d04f9cc2dabaf
Dong <jellylidong@users.noreply.github.com>
2016-06-27 23:32:19 -0400

    [266. Palindrome Permutation][Accepted]committed by LeetCode Extension

--- 266-Palindrome-Permutation/solution.java
+++ 266-Palindrome-Permutation/solution.java
@@ -1,11 +1,15 @@
 public class Solution {
     public boolean canPermutePalindrome(String s) {
-        int[] count = new int[26];
+        HashMap<Character, Integer> map = new HashMap<>();
         for(int i = 0; i < s.length(); i++){
-            count[s.charAt(i)-'a']++;
+            char c = s.charAt(i);
+            if(map.containsKey(c))
+                map.put(c, map.get(c)+1);
+            else
+                map.put(c, 1);
         }
         int oddCount = 0;
-        for(int n: count){
+        for(int n: map.values()){
             if(n%2 != 0)
                 oddCount++;
         }
