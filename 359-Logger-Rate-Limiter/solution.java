ec89c08453fe384fea0afdccb079d7725d66b4d8
Dong <jellylidong@users.noreply.github.com>
2016-06-28 18:12:24 -0400

    [359. Logger Rate Limiter][Accepted]committed by LeetCode Extension

--- 359-Logger-Rate-Limiter/solution.java
+++ 359-Logger-Rate-Limiter/solution.java
@@ -1,17 +1,14 @@
 public class Logger {
 
-    /** Initialize your data structure here. */
-    
-    HashMap<Integer, HashSet<String>> map;
+/**
+HashMap<Integer, HashSet<String>> map;
     Queue<Integer> q;
     public Logger() {
         map = new HashMap<>();    
         q = new LinkedList<>();
     }
     
-    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
-        If this method returns false, the message will not be printed.
-        The timestamp is in seconds granularity. */
+    /
     public boolean shouldPrintMessage(int timestamp, String message) {
         if(!q.isEmpty() && q.peek() == timestamp){
             if(map.get(timestamp).contains(message))
@@ -32,12 +29,33 @@ public class Logger {
         
         return true;
     }
-}
+**/
 
-/*
-["Logger","shouldPrintMessage","shouldPrintMessage","shouldPrintMessage","shouldPrintMessage","shouldPrintMessage","shouldPrintMessage","shouldPrintMessage","shouldPrintMessage","shouldPrintMessage","shouldPrintMessage"]
-[[],[0,"A"],[0,"B"],[0,"C"],[0,"A"],[0,"B"],[0,"C"],[0,"A"],[0,"B"],[0,"C"],[0,"A"]]
-*/
+//above code works and saves space, but slows down the performance when clearing the space
+    /** Initialize your data structure here. */
+    HashMap<String, Integer> map;
+    public Logger() {
+        map = new HashMap<>();
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if(!map.containsKey(message)){
+            map.put(message, timestamp);
+            return true;
+        }
+        else{
+            if(timestamp >= map.get(message)+10){
+                map.put(message, timestamp);
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
 
 /**
  * Your Logger object will be instantiated and called as such:
