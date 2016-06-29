8a731e656fbd903d93893c3c338dbbd1fe7b1ef8
Dong <jellylidong@users.noreply.github.com>
2016-06-29 17:33:52 -0400

    [81. Search in Rotated Sorted Array II][Accepted]committed by LeetCode Extension

--- 81-Search-in-Rotated-Sorted-Array-II/solution.java
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.java
@@ -1,32 +1,58 @@
 public class Solution {
     public boolean search(int[] nums, int target) {
+        // int lo = 0;
+        // int hi = nums.length-1;
+        
+        // while(lo < hi){
+        //     int mid = lo + (hi-lo)/2;
+            
+        //     if(nums[mid] == target)
+        //         return true;
+                
+        //     if(nums[lo] < nums[mid]){
+        //         if(nums[lo] <= target && target < nums[mid])
+        //             hi = mid-1;
+        //         else
+        //             lo = mid+1;
+        //     }
+        //     else if(nums[lo] == nums[mid])
+        //         lo++;
+        //     else if(nums[mid] == nums[hi])
+        //         hi--;
+        //     else{
+        //         if(nums[mid] < target && target <= nums[hi])
+        //             lo = mid+1;
+        //         else
+        //             hi = mid-1;
+        //     }
+        // }
+        
+        // return nums[lo] == target;
+        
+        //just type again
         int lo = 0;
         int hi = nums.length-1;
         
         while(lo < hi){
             int mid = lo + (hi-lo)/2;
-            
             if(nums[mid] == target)
                 return true;
                 
-            if(nums[lo] < nums[mid]){
+            else if(nums[lo] == nums[hi])
+                hi--;
+            else if(nums[lo] <= nums[mid]){
                 if(nums[lo] <= target && target < nums[mid])
                     hi = mid-1;
                 else
                     lo = mid+1;
             }
-            else if(nums[lo] == nums[mid])
-                lo++;
-            else if(nums[mid] == nums[hi])
-                hi--;
-            else{
+            else if(nums[lo] > nums[mid]){
                 if(nums[mid] < target && target <= nums[hi])
                     lo = mid+1;
                 else
                     hi = mid-1;
             }
         }
-        
         return nums[lo] == target;
     }
 }
