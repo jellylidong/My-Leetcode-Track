46261b001b3c864a7b678464e88d82c5f2c242fd
Dong <jellylidong@users.noreply.github.com>
2016-06-29 18:15:10 -0400

    [24. Swap Nodes in Pairs][Accepted]committed by LeetCode Extension

--- 24-Swap-Nodes-in-Pairs/solution.java
+++ 24-Swap-Nodes-in-Pairs/solution.java
@@ -8,20 +8,41 @@
  */
 public class Solution {
     public ListNode swapPairs(ListNode head) {
+        // ListNode helper = new ListNode(0);
+        // helper.next = head;
+        // ListNode pre = helper;
+        // ListNode cur = head;
+        
+        // while(cur != null){
+        //     ListNode next = cur.next;
+        //     if(next == null)
+        //         break;
+        //     pre.next = next;
+        //     cur.next = next.next;
+        //     next.next = cur;
+        //     pre = cur;
+        //     cur = cur.next;
+        // }
+        
+        // return helper.next;
+        
+        //just type again
         ListNode helper = new ListNode(0);
         helper.next = head;
-        ListNode pre = helper;
         ListNode cur = head;
+        ListNode pre = helper;
         
         while(cur != null){
             ListNode next = cur.next;
-            if(next == null)
+            if(next != null){
+                cur.next = next.next;
+                pre.next = next;
+                next.next = cur;
+                pre = cur;
+                cur = cur.next;
+            }
+            else
                 break;
-            pre.next = next;
-            cur.next = next.next;
-            next.next = cur;
-            pre = cur;
-            cur = cur.next;
         }
         
         return helper.next;
