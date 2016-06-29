79dcd668d935e497d4463b0ac1b7066ddbd74de7
Dong <jellylidong@users.noreply.github.com>
2016-06-29 13:44:28 -0400

    [54. Spiral Matrix][Accepted]committed by LeetCode Extension

--- 54-Spiral-Matrix/solution.java
+++ 54-Spiral-Matrix/solution.java
@@ -1,40 +1,80 @@
 public class Solution {
     public List<Integer> spiralOrder(int[][] m) {
-        if(m.length == 0)
-            return new ArrayList<Integer>();
-        if(m[0].length == 0)
-            return new ArrayList<Integer>();
+        // if(m.length == 0)
+        //     return new ArrayList<Integer>();
+        // if(m[0].length == 0)
+        //     return new ArrayList<Integer>();
             
+        // int left = 0;
+        // int up = 0;
+        // int right = m[0].length-1;
+        // int down = m.length-1;
+        
+        // List<Integer> ans = new ArrayList<>();
+        // while(left <= right && up <= down){
+        //     for(int i = left; i <= right; i++)
+        //         ans.add(m[up][i]);
+        //     up++;
+            
+        //     for(int i = up; i <= down; i++)
+        //         ans.add(m[i][right]);
+        //     right--;
+            
+        //     //when going left, must ganrantee current row  is valid
+        //     if(up <= down)
+        //         for(int i = right; i >= left; i--)
+        //             ans.add(m[down][i]);
+        //     down--;
+            
+        //     //when going up, must ganrantee current col is valid
+        //     if(left <= right)
+        //         for(int i = down; i >= up; i--)
+        //             ans.add(m[i][left]);
+        //     left++;
+            
+        //     //otherwise for cases like [[2,3]] (which is only one row or one col), we can get wrong answer, add too many numbers
+        // }
+        
+        // return ans;
+        List<Integer> ans = new ArrayList<>();
+        int down = m.length-1;
+        if(down < 0)
+            return ans;
+        int right = m[0].length-1;
+        if(right < 0)
+            return ans;
         int left = 0;
         int up = 0;
-        int right = m[0].length-1;
-        int down = m.length-1;
-        
-        List<Integer> ans = new ArrayList<>();
+
         while(left <= right && up <= down){
-            for(int i = left; i <= right; i++)
-                ans.add(m[up][i]);
-            up++;
-            
-            for(int i = up; i <= down; i++)
-                ans.add(m[i][right]);
-            right--;
-            
-            //when going left, must ganrantee current row  is valid
-            if(up <= down)
+            //go right
+            if(up <= down){
+                for(int i = left; i <= right; i++)
+                    ans.add(m[up][i]);
+                up++;
+            }
+
+            //go down
+            if(left <= right){
+                for(int i = up; i <= down; i++)
+                    ans.add(m[i][right]);
+                right--;
+            }
+
+            //go left
+            if(up <= down){
                 for(int i = right; i >= left; i--)
                     ans.add(m[down][i]);
-            down--;
-            
-            //when going up, must ganrantee current col is valid
-            if(left <= right)
+                down--;
+            }
+
+            //go up
+            if(left <= right){
                 for(int i = down; i >= up; i--)
                     ans.add(m[i][left]);
-            left++;
-            
-            //otherwise for cases like [[2,3]] (which is only one row or one col), we can get wrong answer, add too many numbers
+                left++;
+            }
         }
-        
         return ans;
     }
 }
