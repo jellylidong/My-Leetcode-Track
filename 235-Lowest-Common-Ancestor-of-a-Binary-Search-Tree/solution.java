ccfc362b0f8896a34badd9c22e7d7ed8327aa84d
Dong <jellylidong@users.noreply.github.com>
2016-06-30 01:42:20 -0400

    [235. Lowest Common Ancestor of a Binary Search Tree][Accepted]committed by LeetCode Extension

--- 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/solution.java
+++ 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/solution.java
@@ -9,30 +9,45 @@
  */
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        TreeNode small = p;
-        TreeNode big = q;
         
+        //make p the smaller node and q the bigger node
         if(p.val > q.val){
-            small = q;
-            big = p;
+            TreeNode tmp = p;
+            p = q;
+            q = tmp;
         }
-        return helper(root, small, big);
+        
+        while(true){
+            if(p == root || q == root || root == null)
+                return root;
+            if(q.val < root.val){
+                root = root.left;
+            }
+            else if(p.val > root.val){
+                root = root.right;
+            }
+            else
+                return root;
+        }
+        
+        
+        // return helper(root, small, big);
         
         
     }
     
-    public TreeNode helper(TreeNode root, TreeNode p, TreeNode q){
-        if(p == root || q == root || root == null)
-            return root;
-        if(q.val < root.val){
-            return helper(root.left, p, q);
-        }
-        else if(p.val > root.val){
-            return helper(root.right, p, q);
-        }
-        else
-            return root;
-    }
+    // public TreeNode helper(TreeNode root, TreeNode p, TreeNode q){
+    //     if(p == root || q == root || root == null)
+    //         return root;
+    //     if(q.val < root.val){
+    //         return helper(root.left, p, q);
+    //     }
+    //     else if(p.val > root.val){
+    //         return helper(root.right, p, q);
+    //     }
+    //     else
+    //         return root;
+    // }
     
     //[2,1,3] node with value 1 node with value 3
 }
