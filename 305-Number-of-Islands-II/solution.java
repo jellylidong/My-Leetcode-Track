afea05c3ccfa9c7c111a550388a1b646ddfc651e
Dong <jellylidong@users.noreply.github.com>
2016-06-30 15:09:56 -0400

    [305. Number of Islands II][Accepted]committed by LeetCode Extension

--- 305-Number-of-Islands-II/solution.java
+++ 305-Number-of-Islands-II/solution.java
@@ -1,81 +1,75 @@
 public class Solution {
     public List<Integer> numIslands2(int m, int n, int[][] positions) {
-        
-        ArrayList<Integer> ans = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
         Union union = new Union(m, n);
-        
-        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
-        
+        int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+
         for(int[] p: positions){
-            int i = p[0];
-            int j = p[1];
-            int p0 = union.transPosition(i, j);
-            union.add(i, j);
+            int i1 = p[0];
+            int j1 = p[1];
+            union.add(i1, j1);
             for(int[] d: dir){
-                int i1 = i+d[0];
-                int j1 = j+d[1];
-                int p1 = union.transPosition(i1, j1);
-                if(p1 >= 0 && union.map.containsKey(p1) && !union.isCnted(p0, p1)){
-                    union.connect(p0, p1);
-                }
+                int i2 = i1+d[0];
+                int j2 = j1+d[1];
+                if(union.isValid(i2,j2) && union.map.containsKey(i2*n+j2) && !union.isCnted(i1,j1, i2,j2))
+                    union.connect(i1,j1, i2,j2);
             }
             ans.add(union.size);
         }
+
         return ans;
     }
-    
+
     public class Union{
-        int size;
         int m, n;
-        //n is board width ie grid[0].length
-        HashMap<Integer, Integer> map; 
-        // key-value is child-fater, positon (i, j) is at position i*n+j
-        //position -1 will be used to indicate invalid position
-        
+        int size;
+        HashMap<Integer, Integer> map; //key-value is child-father
+
         Union(int m, int n){
             this.m = m;
             this.n = n;
             this.size = 0;
-            //m, n  will be used for calculating valid positions
-            map = new HashMap<>();
+            this.map = new HashMap<>();
         }
-        
+
         public void add(int i, int j){
-            int p = transPosition(i, j);
-            if(p != -2){
-                map.put(p, p);
+            if(isValid(i, j)) {
+                map.put(i * n + j, i * n + j);
                 size++;
             }
         }
-        
-        public int transPosition(int i, int j){ // transform (i, j) to i*n+j
-            if(i < 0 || i >= m || j < 0 || j >= n)
-                return -2; //invalid position
-            else
-                return i*n+j;
+
+        public boolean isValid(int i, int j){
+            return (i >= 0 && i < m) && (j >= 0 && j < n);
         }
-        
-        public boolean isCnted(int p1, int p2){
-            return root(p1) == root(p2);
-        }
-        
-        public int root(int p){
+
+        public int root(int i, int j){
+            int p = i*n + j;
             while(p != map.get(p)){
-                int father = map.get(map.get(p));//p's father's father
+                int father = map.get(map.get(p)); //p's father's fathers
                 map.put(p, father);
-                p = map.get(p);
+                p = map.get(p); // or p = father
             }
             return p;
         }
-        
-        public void connect(int p1, int p2){
-            int r1 = root(p1);
-            int r2 = root(p2);
-            if(r1 > r2){
-                map.put(r2, r1);
-            }
-            else{
+
+        public boolean isCnted(int i1, int j1, int i2, int j2){
+            return root(i1, j1) == root(i2, j2);
+        }
+
+        public void connect(int i1, int j1, int i2, int j2){
+            int p1 = i1*n+j1;
+            int p2 = i2*n+j2;
+
+            int r1 = root(i1, j1);
+            int r2 = root(i2, j2);
+            if(r1 < r2) { //here we assume bigger number is father
                 map.put(r1, r2);
+
+            }
+            else {
+                map.put(r2, r1);
+
             }
             size--;
         }
