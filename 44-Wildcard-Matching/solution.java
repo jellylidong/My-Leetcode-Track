262cded3aba48ad574833042b1cf09ea42271c95
Dong <jellylidong@users.noreply.github.com>
2016-07-04 14:05:25 -0400

    [44. Wildcard Matching][Runtime Error]committed by LeetCode Extension

--- 44-Wildcard-Matching/solution.java
+++ 44-Wildcard-Matching/solution.java
@@ -1,50 +1,37 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
+        
         int lo = 0;
-        while(lo < p.length() && p.charAt(lo) == '*')
+        while(lo < p.length() && p.charAt(lo) == '*'){
             lo++;
-        lo = lo==0? lo : lo-1;
+        }
+        if(lo > 0)  lo--;
         
         int hi = p.length()-1;
-        while(hi > lo && p.charAt(hi) == '*')
+        while(lo > hi && p.charAt(hi) == '*'){
             hi--;
-        hi = hi == p.length()-1? hi :hi+1;
-            
+        }
+        if(hi < p.length()-1) hi++;
+        
         p = p.substring(lo, hi+1);
-        // System.out.println(p);
         
         int ls = s.length();
         int lp = p.length();
-        
-        boolean[][] dp = new boolean[ls+1][lp+1];
-        dp[ls][lp] = true; // empty matches empty
-        if(lp-1 >= 0)
-            dp[ls][lp-1] = p.charAt(lp-1) == '*'; // empty matches "*"
-        //do[i][j] means s.substring(i, ls) matches p.substring(j, lp);
-        
-        
-        //why do we go from back?
-        //since * can match any sequence,
-        //if we go from left to right, 
-        //we won't be able to know how many chars we should used to match *
+        boolean[][] dp = new boolean[ls+1][lp+1]; // s.substring(i, ls) matches p.substring(j, lp)
+        dp[ls][lp] = true;
         for(int i = ls-1; i >= 0; i--){
             for(int j = lp-1; j >= 0; j--){
                 if(s.charAt(i) == p.charAt(j))
                     dp[i][j] = dp[i+1][j+1];
                 else if(p.charAt(j) == '?')
-                    dp[i][j] = dp[i+1][j+1];
-                else if(p.charAt(j) == '*'){
-                    dp[i][j] = dp[i+1][j] 
-                            // if "XXX" matches "*XXX",  then "XXXX" also matches "*XXX",
-                            // *'s sequence add another char
-                            || dp[i][j+1];
-                            //if current s "XXX" match current p "XXX"
-                            //then p becomes "*XXX", let * work as empty string, then s "XXX" matches p "*XXX"
-                }
+                     dp[i][j] = dp[i+1][j+1];
+                else if(p.charAt(j) == '*')
+                    dp[i][j] = dp[i+1][j] || dp[i][j+1];// * works as a string || * works as empty
             }
         }
         
         return dp[0][0];
+        
         
     }
 }
