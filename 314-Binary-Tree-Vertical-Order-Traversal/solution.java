9ab9e1b7fba6980be0b9775402761b824ccdb99c
Dong <jellylidong@users.noreply.github.com>
2016-07-04 20:48:48 -0400

    [314. Binary Tree Vertical Order Traversal][Runtime Error]committed by LeetCode Extension

--- 314-Binary-Tree-Vertical-Order-Traversal/solution.java
+++ 314-Binary-Tree-Vertical-Order-Traversal/solution.java
@@ -9,70 +9,38 @@
  */
 public class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if(root == null)
-            return ans;
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        traverse(root, map);
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        int left = 0, right = 0;
+        TreeNode cur = root.left;
+        while(cur != null){
+            left++;
+            cur = cur.left;
+        }
+        cur = root.right;
+        while(cur != null){
+            right++;
+            cur = cur.right;
+        }
         
-        List<Integer> levelList = new ArrayList<>(map.keySet());
-        Collections.sort(levelList);
-        for(int level: levelList)
-            ans.add(map.get(level));
-            
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0; i < left+right+1; i++)
+            ans.add(new ArrayList<Integer>());
+        add(root, 0, map);
+        for(int i: map.keySet()){
+            ans.set(left+i, map.get(i));
+        }
         return ans;
         
     }
     
-    public void traverse(TreeNode root,  HashMap<Integer, List<Integer>> map){
-        // if(map.containsKey(level))
-        //     map.get(level).add(root.val);
-        // else{
-        //     List<Integer> list = new ArrayList<>();
-        //     list.add(root.val);
-        //     map.put(level, list);
-        // }
-        
-        // if(root.left != null)
-        //     traverse(root.left, level-1, map);
-        // if(root.right != null)
-        //     traverse(root.right, level+1, map);
+    public void add(TreeNode root, int level, HashMap<Integer, ArrayList<Integer>> map){
+        if(root == null)
+            return;
+        if(!map.containsKey(level))
+            map.put(level, new ArrayList<Integer>());
             
-        //input is [3,9,8,4,0,1,7,null,null,null,2,5]
-        //the above code generate 
-        //[[4],[9,5],[3,0,1],[2,8],[7]]
-        //but correct answer is
-        //[[4],[9,5],[3,0,1],[8,2],[7]]
-        //so it should be BFS instead of DFS
-        
-        Queue<pair> q = new LinkedList<>();
-        q.offer(new pair(root, 0));
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i = 0; i < size; i++){
-                pair cur = q.poll();
-                if(map.containsKey(cur.level))
-                    map.get(cur.level).add(cur.node.val);
-                else{
-                    List<Integer> list = new ArrayList<>();
-                    list.add(cur.node.val);
-                    map.put(cur.level, list);
-                }
-                if(cur.node.left != null)
-                    q.offer(new pair(cur.node.left, cur.level-1));
-                if(cur.node.right != null)
-                    q.offer(new pair(cur.node.right, cur.level+1));
-            }
-        }
-        
-    }
-    
-    public class pair{
-        TreeNode node;
-        int level;
-        pair(TreeNode node, int level){
-            this.node = node;
-            this.level = level;
-        }
+        map.get(level).add(root.val);
+        add(root.left, level-1, map);
+        add(root.right, level+1, map);
     }
 }
