6dcead075b6b884c26e964612927cd79d94332ff
Dong <jellylidong@users.noreply.github.com>
2016-07-04 20:27:37 -0400

    [289. Game of Life][Accepted]committed by LeetCode Extension

--- 289-Game-of-Life/solution.java
+++ 289-Game-of-Life/solution.java
@@ -1,52 +1,47 @@
 public class Solution {
     
-   //trick part of this problem, use bit to indicate multi states, when each state has two status 
-   //state1 has 0,1, state2 has 0,1 so state1 and state2 can be denote as 11 00 10 01 and so forth
+    //current status only uses one bit, we can use its other bit to indicate other state
+    //for example, in this problem, most right bit is current state, then 2nd right bit is next state, 
+    //and so forth
     
     public void gameOfLife(int[][] board) {
-        if(board.length == 0 || board[0].length == 0)
-            return;
-            
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                int lives = lives(board, i, j);
-                //current state is live
+        for(int i=0; i < board.length; i++){
+            for(int j=0; j < board[0].length; j++){
+                int l = lives(board, i, j);
                 if((board[i][j]&1) == 1){
-                    if(lives > 3 || lives < 2)
-                        board[i][j] |= 0;
-                    else
-                        board[i][j] |= 2;
+                    if(l < 2) board[i][j] &= 1;
+                    else if(l==2 || l==3) board[i][j] |= 2;
+                    else if(l > 3)  board[i][j] &= 1;
                 }
-                //current state is dead
                 else{
-                    if(lives == 3)
+                    if(l == 3)
                         board[i][j] |= 2;
                 }
             }
         }
         
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                board[i][j] >>= 1;    
+        for(int i=0; i < board.length; i++){
+            for(int j=0; j < board[0].length; j++){
+                board[i][j] >>= 1;
             }
         }
     }
     
     public int lives(int[][] board, int i0, int j0){
-        int lives = 0;
-        for(int i = Math.max(0, i0-1); i <= Math.min(board.length-1, i0+1); i++){
-            for(int j = Math.max(0, j0-1); j <= Math.min(board[0].length-1, j0+1); j++){
-                if((board[i][j]&1) == 1)
-                    lives++;
+        int res = 0;
+        for(int i = -1; i <= 1; i++){
+            for(int j = -1; j <= 1; j++){
+                if(i0+i >= 0 && i0+i < board.length && j0+j >= 0 && j0+j < board[0].length){
+                    if((board[i0+i][j0+j]&1) == 1)
+                        res++;
+                }
             }
         }
         
-        //Don't forget to reduce itself
         if((board[i0][j0]&1) == 1)
-            lives--;
-        
-        return lives;
+            res--;
+            
+        return res;
     }
     
-   
 }
