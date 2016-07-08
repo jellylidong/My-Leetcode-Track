7f70fe9edd9479aa9f6bf4c7dea74e8471862577
Dong <jellylidong@users.noreply.github.com>
2016-07-07 19:14:31 -0700

    [253. Meeting Rooms II][Accepted]committed by LeetCode Extension

--- 253-Meeting-Rooms-II/solution.java
+++ 253-Meeting-Rooms-II/solution.java
@@ -11,19 +11,25 @@ public class Solution {
     public int minMeetingRooms(Interval[] its) {
         if(its.length == 0)
             return 0;
-        Arrays.sort(its, new comparatorStart());
+        Arrays.sort(its, new comparatorStart());//sort meetings according to its start time
+        
         PriorityQueue<Interval> pq = new PriorityQueue<>(its.length, new comparatorEnd());
-        int count = 0;
         pq.offer(its[0]);
         
         for(int i = 1; i < its.length; i++){
+            //find the meeting that finishes earliest
             Interval interval = pq.poll();
             if(its[i].start >= interval.end){
+                //if current meeting starts after the earliest finished meeting
+                //there's no need to add a new room
+                //just mere the interval
                 interval.end = its[i].end;
             }
             else{
+                //otherwise, we need to a new meeting room 
                 pq.offer(its[i]);
             }
+            //also don't forget to put the previous meeting room back to the pq
             pq.offer(interval);
         }
         
