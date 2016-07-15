2924255ecda37215cc2634e8e8f303e457a62426
Dong <jellylidong@users.noreply.github.com>
2016-07-14 21:45:39 -0700

    [286. Walls and Gates][Accepted]committed by LeetCode Extension

--- 286-Walls-and-Gates/solution.java
+++ 286-Walls-and-Gates/solution.java
@@ -3,26 +3,29 @@ public class Solution {
         for(int i = 0; i < rooms.length; i++){
             for(int j = 0; j < rooms[0].length; j++){
                 if(rooms[i][j] == 0){
-                    update(rooms, i-1, j, 0);
-                    update(rooms, i+1, j, 0);
-                    update(rooms, i, j-1, 0);
-                    update(rooms, i, j+1, 0);
+                    update(i-1, j, 0, rooms);
+                    update(i+1, j, 0, rooms);
+                    update(i, j-1, 0, rooms);
+                    update(i, j+1, 0, rooms);
                 }
             }
         }
     }
     
-    public void update(int[][] rooms, int i, int j, int pre){
-        if(i < 0 || i >= rooms.length || j < 0 || j >= rooms[0].length)
+    public void update(int i, int j, int pre, int[][] rooms){
+        if(i < 0 || i == rooms.length || j < 0 || j == rooms[0].length){
             return;
-        if(rooms[i][j] != 0 && rooms[i][j] != -1){
-            if(rooms[i][j] > pre + 1){
-                rooms[i][j] = pre+1;
-                update(rooms, i-1, j, pre+1);
-                update(rooms, i+1, j, pre+1);
-                update(rooms, i, j-1, pre+1);
-                update(rooms, i, j+1, pre+1);
-
+        }
+        else{
+            if(rooms[i][j] != -1 && rooms[i][j] != 0){
+                if(rooms[i][j] > pre+1){
+                    // System.out.println(i + " " + j);
+                    rooms[i][j] = pre+1;
+                    update(i-1, j, pre+1, rooms);
+                    update(i+1, j, pre+1, rooms);
+                    update(i, j-1, pre+1, rooms);
+                    update(i, j+1, pre+1, rooms);
+                }
             }
         }
     }
